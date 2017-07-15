3de7e58ef7f538c461dbd5590f79bcb0e214c975
gilcrest <gilcrest@gmail.com>
2017-07-15 01:08:54 -0400

    refactored to use type method for handler to allow to pass parameters into handler func

--- cmd/appUser/server/main.go
+++ cmd/appUser/server/main.go
@@ -17,8 +17,6 @@ import (
 	"go.uber.org/zap"
 )
 
-var environment env.Env
-
 type testUser struct {
 	Username  string
 	MobileID  string
@@ -30,7 +28,9 @@ type testUser struct {
 func main() {
 
 	// Initializes "environment" object to be passed around to functions
-	envInit()
+	// func envInit() *env.Env
+	env := envInit()
+	tsth := &testHandler{env: env}
 
 	// create a new mux (multiplex) router
 	// func NewRouter() *Router
@@ -39,7 +39,7 @@ func main() {
 	// API may have multiple versions and the matching may get a bit
 	// lengthy, this routeMatcher function helps with organizing that
 	// func routeMatcher(rtr *mux.Router) *mux.Router
-	r = routeMatcher(r)
+	r = routeMatcher(tsth, r)
 
 	// handle all requests with the Gorilla router by adding
 	// r to the DefaultServeMux
@@ -53,7 +53,7 @@ func main() {
 }
 
 // Initializes "environment" object to be passed around to functions
-func envInit() {
+func envInit() *env.Env {
 
 	logger, _ := zap.NewProduction()
 
@@ -65,52 +65,52 @@ func envInit() {
 		log.Fatal(err)
 	}
 
-	environment = env.Env{Db: sqldb, Logger: logger}
+	environment := &env.Env{Db: sqldb, Logger: logger}
+
+	return environment
 
 }
 
-func routeMatcher(rtr *mux.Router) *mux.Router {
+func routeMatcher(th *testHandler, rtr *mux.Router) *mux.Router {
 
 	// match only POST requests on /api/appUser/create
 	// This is the original (v1) version for the API and the response for this will never change
 	//  with versioning in order to maintain a stable contract
 	// func (r *Router) HandleFunc(path string, f func(http.ResponseWriter, *http.Request)) *Route
-	rtr.HandleFunc("/api/appUser/create", createUserHandler).
+	rtr.HandleFunc("/api/appUser/create", th.createUserHandler).
 		Methods("POST").
 		Headers("Content-Type", "application/json")
 
 	// match only POST requests on /api/v1/appUser/create
 	// func (r *Router) HandleFunc(path string, f func(http.ResponseWriter, *http.Request)) *Route
-	rtr.HandleFunc("/api/v1/appUser/create", createUserHandler).
-		Methods("POST").
-		Headers("Content-Type", "application/json")
+	//rtr.HandleFunc("/api/v1/appUser/create", createUserHandler).
+	//	Methods("POST").
+	//	Headers("Content-Type", "application/json")
 
 	return rtr
 }
 
-/*
-Creates a user in the database, but also:
-	- writes a log of the request and response
-	- "pretty prints" the request
-*/
-func createUserHandler(w http.ResponseWriter, req *http.Request) {
+type testHandler struct {
+	env *env.Env
+}
 
+func (th *testHandler) createUserHandler(w http.ResponseWriter, req *http.Request) {
 	// retrieve the context from the http.Request
 	ctx := req.Context()
-	logger := environment.Logger
+	logger := th.env.Logger
 	logger.Debug("handleMbrLog started")
 
-	defer environment.Logger.Sync()
+	defer th.env.Logger.Sync()
 	defer logger.Debug("handleMbrLog ended")
 
-	logRequest(req)
+	//logRequest(req)
 	prettyPrintRequest(req)
 
 	// db.NewContext function creates and begins a new sql.Tx, which pulls from the
 	// previously opened database (postgres) connection pool and starts a database
 	// transaction.  In addition, the pointer to this "started" sql.Tx is included
 	// in the above created context
-	ctx = db.AddDBTx2Context(ctx, environment, nil)
+	ctx = db.AddDBTx2Context(ctx, th.env, nil)
 
 	decoder := json.NewDecoder(req.Body)
 	var t testUser
@@ -141,6 +141,60 @@ func createUserHandler(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+//func createUserHandler(w http.ResponseWriter, req *http.Request) {
+//
+//	// retrieve the context from the http.Request
+//	ctx := req.Context()
+//	logger := environment.Logger
+//	logger.Debug("handleMbrLog started")
+//
+//	defer environment.Logger.Sync()
+//	defer logger.Debug("handleMbrLog ended")
+//
+//	logRequest(req)
+//	prettyPrintRequest(req)
+//
+//	// db.NewContext function creates and begins a new sql.Tx, which pulls from the
+//	// previously opened database (postgres) connection pool and starts a database
+//	// transaction.  In addition, the pointer to this "started" sql.Tx is included
+//	// in the above created context
+//	ctx = db.AddDBTx2Context(ctx, environment, nil)
+//
+//	decoder := json.NewDecoder(req.Body)
+//	var t testUser
+//	err := decoder.Decode(&t)
+//	if err != nil {
+//		panic(err)
+//	}
+//	defer req.Body.Close()
+//	log.Println(t)
+//
+//	inputUsr := appUser.User{Username: "repoMan", MobileID: "(617) 302-7777", Email: "repoman@alwaysintense.com", FirstName: "Otto", LastName: "Maddox"}
+//	//auditUsr := appUser.User{Username: "bud", MobileID: "(617) 302-7777", Email: "repoman@alwaysintense.com", FirstName: "Otto", LastName: "Maddox"}
+//
+//	// Call the create method of the appUser object to validate data and write to db
+//	logsWritten, err := inputUsr.Create(ctx)
+//
+//	fmt.Fprintf(w, "logsWritten = %d\n", logsWritten)
+//
+//	tx, ok := db.DBTxFromContext(ctx)
+//
+//	if ok && logsWritten > 0 {
+//		err = tx.Commit()
+//		if err != nil {
+//			log.Fatal(err)
+//		}
+//	} else if logsWritten <= 0 {
+//		log.Fatal(err)
+//	}
+//}
+
+/*
+Creates a user in the database, but also:
+	- writes a log of the request and response
+	- "pretty prints" the request
+*/
+
 func prettyPrintRequest(req *http.Request) {
 	// Save a copy of this request for debugging.
 	requestDump, err := httputil.DumpRequest(req, true)
@@ -150,45 +204,45 @@ func prettyPrintRequest(req *http.Request) {
 	fmt.Println(string(requestDump))
 }
 
-func logRequest(req *http.Request) {
-
-	// type Request struct {
-	//            Method string
-	//            URL *url.URL
-	//            Proto      string // "HTTP/1.0"
-	//            ProtoMajor int    // 1
-	//            ProtoMinor int    // 0
-	//            Header Header
-	//            Body io.ReadCloser
-	//            ContentLength int64
-	//            TransferEncoding []string
-	//            Close bool
-	//            Host string
-	//            Form url.Values
-	//            PostForm url.Values
-	//            MultipartForm *multipart.Form
-	//            Trailer Header
-	//            RemoteAddr string
-	//            RequestURI string
-	//            TLS *tls.ConnectionState
-	//    }
-
-	logger := environment.Logger
-	logger.Info("Request received",
-		zap.String("URL Path", req.URL.Path[1:]),
-		zap.String("HTTP method", req.Method),
-		zap.String("URL", req.URL.String()),
-		zap.String("Protocol", req.Proto),
-		zap.Int("ProtoMajor", req.ProtoMajor),
-		zap.Int("ProtoMinor", req.ProtoMinor),
-
-		//TODO - finish logging the rest of the request
-		//fmt.Fprintf(w, "Header = %s\n", req.Header)
-		//fmt.Fprintf(w, "Body = %s\n", req.Body)
-		//fmt.Fprintf(w, "Content Length = %d\n", req.ContentLength)
-		//fmt.Fprintf(w, "Transfer Encoding = %s\n", req.TransferEncoding)
-		//fmt.Fprintf(w, "Close boolean = %t\n", req.Close)
-		//fmt.Fprintf(w, "Host = %s\n", req.Host)
-		//fmt.Fprintf(w, "Post Form Values = %s\n", req.Form)
-	)
-}
+//func logRequest(req *http.Request) {
+//
+//	// type Request struct {
+//	//            Method string
+//	//            URL *url.URL
+//	//            Proto      string // "HTTP/1.0"
+//	//            ProtoMajor int    // 1
+//	//            ProtoMinor int    // 0
+//	//            Header Header
+//	//            Body io.ReadCloser
+//	//            ContentLength int64
+//	//            TransferEncoding []string
+//	//            Close bool
+//	//            Host string
+//	//            Form url.Values
+//	//            PostForm url.Values
+//	//            MultipartForm *multipart.Form
+//	//            Trailer Header
+//	//            RemoteAddr string
+//	//            RequestURI string
+//	//            TLS *tls.ConnectionState
+//	//    }
+//
+//	logger := environment.Logger
+//	logger.Info("Request received",
+//		zap.String("URL Path", req.URL.Path[1:]),
+//		zap.String("HTTP method", req.Method),
+//		zap.String("URL", req.URL.String()),
+//		zap.String("Protocol", req.Proto),
+//		zap.Int("ProtoMajor", req.ProtoMajor),
+//		zap.Int("ProtoMinor", req.ProtoMinor),
+//
+//		//TODO - finish logging the rest of the request
+//		//fmt.Fprintf(w, "Header = %s\n", req.Header)
+//		//fmt.Fprintf(w, "Body = %s\n", req.Body)
+//		//fmt.Fprintf(w, "Content Length = %d\n", req.ContentLength)
+//		//fmt.Fprintf(w, "Transfer Encoding = %s\n", req.TransferEncoding)
+//		//fmt.Fprintf(w, "Close boolean = %t\n", req.Close)
+//		//fmt.Fprintf(w, "Host = %s\n", req.Host)
+//		//fmt.Fprintf(w, "Post Form Values = %s\n", req.Form)
+//	)
+//}
