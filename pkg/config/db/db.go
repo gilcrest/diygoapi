b24e3a3bfe3a8afcbbeb90f5859dad52d5f4e0aa
gilcrest <gilcrest@gmail.com>
2017-07-29 15:21:32 -0400

    Moved NewDB out to own file to avoid circular reference

--- pkg/config/db/db.go
+++ pkg/config/db/db.go
@@ -7,8 +7,6 @@ package db
 import (
 	"context"
 	"database/sql"
-	"fmt"
-	"os"
 
 	"github.com/gilcrest/go-API-template/pkg/config/env"
 	_ "github.com/lib/pq"
@@ -35,29 +33,6 @@ func DBTxFromContext(ctx context.Context) (*sql.Tx, bool) {
 	// the *sql.Tx type assertion returns ok=false for nil.
 	dbTx, ok := ctx.Value(dbTxKey).(*sql.Tx)
 	return dbTx, ok
-}
-
-// returns an open database handle of 0 or more underlying connections
-func NewDB() (*sql.DB, error) {
-
-	// Get Database connection credentials from environment variables
-	DB_NAME := os.Getenv("PG_DBNAME")
-	DB_USER := os.Getenv("PG_USERNAME")
-	DB_PASSWORD := os.Getenv("PG_PASSWORD")
-
-	// Craft string for database connection
-	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME)
-
-	// Open the postgres database using the postgres driver (pq)
-	// func Open(driverName, dataSourceName string) (*DB, error)
-	db, err := sql.Open("postgres", dbinfo)
-	if err != nil {
-		return nil, err
-	}
-	if err = db.Ping(); err != nil {
-		return nil, err
-	}
-	return db, nil
 }
 
 // Opens a database connection and starts a database transaction using the
