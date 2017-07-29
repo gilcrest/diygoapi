0d1901028c5af69fad8d80db3f025d9f3fac5d2e
gilcrest <gilcrest@gmail.com>
2017-07-29 14:27:10 -0400

    Moved functions and clutter out to different package

--- cmd/appUser/server/main.go
+++ cmd/appUser/server/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"net/http/httputil"
 
 	"github.com/gilcrest/go-API-template/pkg/config/db"
 	"github.com/gilcrest/go-API-template/pkg/config/env"
@@ -16,7 +14,7 @@ import (
 
 func main() {
 
-	// Initializes "environment" object to be passed around to functions
+	// Initializes "environment" type to be passed around to functions
 	// func envInit() *env.Env
 	env := envInit()
 
@@ -27,7 +25,7 @@ func main() {
 	// API may have multiple versions and the matching may get a bit
 	// lengthy, this routeMatcher function helps with organizing that
 	// func routeMatcher(rtr *mux.Router) *mux.Router
-	r = handlers.PathMatcher(env, r)
+	r = handlers.PathMatch(env, r)
 
 	// handle all requests with the Gorilla router by adding
 	// r to the DefaultServeMux
@@ -58,55 +56,3 @@ func envInit() *env.Env {
 	return environment
 
 }
-
-func prettyPrintRequest(req *http.Request) {
-	// Save a copy of this request for debugging.
-	requestDump, err := httputil.DumpRequest(req, true)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(string(requestDump))
-}
-
-//func logRequest(req *http.Request) {
-//
-//	// type Request struct {
-//	//            Method string
-//	//            URL *url.URL
-//	//            Proto      string // "HTTP/1.0"
-//	//            ProtoMajor int    // 1
-//	//            ProtoMinor int    // 0
-//	//            Header Header
-//	//            Body io.ReadCloser
-//	//            ContentLength int64
-//	//            TransferEncoding []string
-//	//            Close bool
-//	//            Host string
-//	//            Form url.Values
-//	//            PostForm url.Values
-//	//            MultipartForm *multipart.Form
-//	//            Trailer Header
-//	//            RemoteAddr string
-//	//            RequestURI string
-//	//            TLS *tls.ConnectionState
-//	//    }
-//
-//	logger := environment.Logger
-//	logger.Info("Request received",
-//		zap.String("URL Path", req.URL.Path[1:]),
-//		zap.String("HTTP method", req.Method),
-//		zap.String("URL", req.URL.String()),
-//		zap.String("Protocol", req.Proto),
-//		zap.Int("ProtoMajor", req.ProtoMajor),
-//		zap.Int("ProtoMinor", req.ProtoMinor),
-//
-//		//TODO - finish logging the rest of the request
-//		//fmt.Fprintf(w, "Header = %s\n", req.Header)
-//		//fmt.Fprintf(w, "Body = %s\n", req.Body)
-//		//fmt.Fprintf(w, "Content Length = %d\n", req.ContentLength)
-//		//fmt.Fprintf(w, "Transfer Encoding = %s\n", req.TransferEncoding)
-//		//fmt.Fprintf(w, "Close boolean = %t\n", req.Close)
-//		//fmt.Fprintf(w, "Host = %s\n", req.Host)
-//		//fmt.Fprintf(w, "Post Form Values = %s\n", req.Form)
-//	)
-//}
