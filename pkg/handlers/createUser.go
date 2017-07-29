35875261fe61d9ecd12f79afa3fefeaa99d7968a
gilcrest <gilcrest@gmail.com>
2017-07-29 14:27:40 -0400

    Deleted

--- pkg/handlers/createUser.go
+++ pkg/handlers/createUser.go
@@ -11,57 +11,8 @@ import (
 	"github.com/gilcrest/go-API-template/pkg/config/env"
 )
 
-// Error represents a handler error. It provides methods for a HTTP status
-// code and embeds the built-in error interface.
-type Error interface {
-	error
-	Status() int
-}
-
-// HTTPStatusError represents an error with an associated HTTP status code.
-type HTTPStatusError struct {
-	Code int
-	Err  error
-}
-
-// Allows HTTPStatusError to satisfy the error interface.
-func (hse HTTPStatusError) Error() string {
-	return hse.Err.Error()
-}
-
-// Returns our HTTP status code.
-func (hse HTTPStatusError) Status() int {
-	return hse.Code
-}
-
-// The Handler struct that takes a configured Env and a function matching
-// our useful signature.
-type Handler struct {
-	Env *env.Env
-	H   func(e *env.Env, w http.ResponseWriter, r *http.Request) error
-}
-
-// ServeHTTP allows our Handler type to satisfy http.Handler.
-func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	err := h.H(h.Env, w, r)
-	if err != nil {
-		switch e := err.(type) {
-		case Error:
-			// We can retrieve the status here and write out a specific
-			// HTTP status code.
-			log.Printf("HTTP %d - %s", e.Status(), e)
-			http.Error(w, e.Error(), e.Status())
-		default:
-			// Any error types we don't specifically look out for default
-			// to serving a HTTP 500
-			http.Error(w, http.StatusText(http.StatusInternalServerError),
-				http.StatusInternalServerError)
-		}
-	}
-}
-
 /*
-Creates a user in the database, but also:
+CreateUserHandler creates a user in the database, but also:
 	- writes a log of the request and response
 	- "pretty prints" the request
 */
@@ -69,13 +20,16 @@ func CreateUserHandler(env *env.Env, w http.ResponseWriter, req *http.Request) e
 	// retrieve the context from the http.Request
 	ctx := req.Context()
 	logger := env.Logger
-	logger.Debug("handleMbrLog started")
+	logger.Debug("CreateUserHandler started")
 
 	defer env.Logger.Sync()
-	defer logger.Debug("handleMbrLog ended")
+	defer logger.Debug("CreateUserHandler ended")
 
-	//logRequest(req)
-	//prettyPrintRequest(req)
+	err := PrintRequest(req)
+
+	if err != nil {
+		return err
+	}
 
 	// db.NewContext function creates and begins a new sql.Tx, which pulls from the
 	// previously opened database (postgres) connection pool and starts a database
@@ -84,7 +38,7 @@ func CreateUserHandler(env *env.Env, w http.ResponseWriter, req *http.Request) e
 	ctx = db.AddDBTx2Context(ctx, env, nil)
 
 	var usr *appUser.User
-	err := json.NewDecoder(req.Body).Decode(&usr)
+	err = json.NewDecoder(req.Body).Decode(&usr)
 	if err != nil {
 		return HTTPStatusError{500, err}
 	}
@@ -112,7 +66,7 @@ func CreateUserHandler(env *env.Env, w http.ResponseWriter, req *http.Request) e
 		// are worth raising a HTTP 500 over vs. which might just be a HTTP
 		// 404, 403 or 401 (as appropriate). It's also clear where our
 		// handler should stop processing by returning early.
-		return HTTPStatusError{500, err}
+		return HTTPStatusError{http.StatusInternalServerError, err}
 	}
 
 	fmt.Fprintf(w, "logsWritten = %d\n", logsWritten)
---
+++ pkg/handlers/logRequest.go
@@ -0,0 +1,62 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+)
+
+// PrintRequest wraps the call to httputil.DumpRequest...
+func PrintRequest(req *http.Request) error {
+
+	// func DumpRequest(req *http.Request, body bool) ([]byte, error)
+	requestDump, err := httputil.DumpRequest(req, true)
+	if err != nil {
+		return HTTPStatusError{http.StatusBadRequest, err}
+	}
+	fmt.Println(string(requestDump))
+	return nil
+}
+
+//func logRequest(req *http.Request) {
+//
+//	// type Request struct {
+//	//            Method string
+//	//            URL *url.URL
+//	//            Proto      string // "HTTP/1.0"
+//	//            ProtoMajor int    // 1
+//	//            ProtoMinor int    // 0
+//	//            Header Header
+//	//            Body io.ReadCloser
+//	//            ContentLength int64
+//	//            TransferEncoding []string
+//	//            Close bool
+//	//            Host string
+//	//            Form url.Values
+//	//            PostForm url.Values
+//	//            MultipartForm *multipart.Form
+//	//            Trailer Header
+//	//            RemoteAddr string
+//	//            RequestURI string
+//	//            TLS *tls.ConnectionState
+//	//    }
+//
+//	logger := environment.Logger
+//	logger.Info("Request received",
+//		zap.String("URL Path", req.URL.Path[1:]),
+//		zap.String("HTTP method", req.Method),
+//		zap.String("URL", req.URL.String()),
+//		zap.String("Protocol", req.Proto),
+//		zap.Int("ProtoMajor", req.ProtoMajor),
+//		zap.Int("ProtoMinor", req.ProtoMinor),
+//
+//		//TODO - finish logging the rest of the request
+//		//fmt.Fprintf(w, "Header = %s\n", req.Header)
+//		//fmt.Fprintf(w, "Body = %s\n", req.Body)
+//		//fmt.Fprintf(w, "Content Length = %d\n", req.ContentLength)
+//		//fmt.Fprintf(w, "Transfer Encoding = %s\n", req.TransferEncoding)
+//		//fmt.Fprintf(w, "Close boolean = %t\n", req.Close)
+//		//fmt.Fprintf(w, "Host = %s\n", req.Host)
+//		//fmt.Fprintf(w, "Post Form Values = %s\n", req.Form)
+//	)
+//}
