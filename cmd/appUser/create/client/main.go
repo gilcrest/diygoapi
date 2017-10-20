e2b87b687c4048cb7f00045f9ce9a0e98761b67d
Dan Gillis <gilcrest@gmail.com>
2017-10-20 06:36:40 -0400

    Added more comments to the code

--- cmd/appUser/create/client/main.go
+++ cmd/appUser/create/client/main.go
@@ -8,26 +8,33 @@ import (
 	"net/url"
 	"time"
 
-	"github.com/gilcrest/go-API-template/pkg/appUser"
-	"github.com/gilcrest/go-API-template/pkg/client"
+	"github.com/gilcrest/go-API-template/pkg/api/client"
+	"github.com/gilcrest/go-API-template/pkg/domain/appUser"
 )
 
 func main() {
-	ctx := context.Background()
-	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+
+	// Initialize an empty context (context.Background()) and then add a
+	//  timeout to it
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
+	// use the Parse method of the URL struct to return a properly formed
+	//  URL struct
 	u, err := url.Parse("http://127.0.0.1:8080/")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var dflt = http.DefaultClient
-	clt := client.UserClient{BaseURL: u, UserAgent: "Gilcrest", HTTPClient: dflt}
+	// Initialize the UserClient struct with the formed URL from above and a pointer to
+	//  then default http client in the http package
+	clt := client.UserClient{BaseURL: u, UserAgent: "Gilcrest", HTTPClient: http.DefaultClient}
 
+	// Initialize an instance of appUser.User
 	usr := appUser.User{Username: "repoMan", MobileID: "(617) 302-7777", Email: "repoman@alwaysintense.com", FirstName: "Otto", LastName: "Maddox"}
 
-	user, err := clt.Create(ctx, usr)
+	// clt.Create does the actual http POST to the endpoint to create an application user
+	user, err := clt.Create(ctx, &usr)
 	if err != nil {
 		if err == context.DeadlineExceeded {
 			log.Fatal("Response timed out, do something different here if you want to...")
