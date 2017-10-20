299b25709716c1062f42b0127c967bc396ea5ac6
Dan Gillis <gilcrest@gmail.com>
2017-10-20 06:46:53 -0400

    Comment cleanup

--- pkg/api/server/handlers/pathMatch.go
+++ pkg/api/server/handlers/pathMatch.go
@@ -11,13 +11,11 @@ func PathMatch(env *env.Env, rtr *mux.Router) *mux.Router {
 	// match only POST requests on /api/appUser/create
 	// This is the original (v1) version for the API and the response for this will never change
 	//  with versioning in order to maintain a stable contract
-	// func (r *Router) HandleFunc(path string, f func(http.ResponseWriter, *http.Request)) *Route
 	rtr.Handle("/api/appUser", Handler{env, CreateUserHandler}).
 		Methods("POST").
 		Headers("Content-Type", "application/json")
 
 	// match only POST requests on /api/v1/appUser/create
-	// func(path string, handler http.Handler) *Route
 	rtr.Handle("/api/v1/appUser", Handler{env, CreateUserHandler}).
 		Methods("POST").
 		Headers("Content-Type", "application/json")
