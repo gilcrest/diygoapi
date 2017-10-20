8dc469a20a4ee4d9e0e875e49323e49123ff6fa1
Dan Gillis <gilcrest@gmail.com>
2017-10-20 06:37:26 -0400

    Added more comments and rollback

--- cmd/appUser/create/debug/main.go
+++ cmd/appUser/create/debug/main.go
@@ -1,4 +1,4 @@
-// Allows for testing the different flows without having to go through
+// Allows for testing the domain logic without having to go through
 // the http server
 package main
 
@@ -22,30 +22,39 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel() // Cancel ctx as soon as main returns.
 
+	// Initializes "environment" struct type
 	env, err := env.NewEnv()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(env)
-	inputUsr := appUser.User{Username: "repoMan", MobileID: "(617) 302-7777", Email: "repoman@alwaysintense.com", FirstName: "Otto", LastName: "Maddox"}
-	// //auditUsr := appUser.User{Username: "bud", MobileID: "(617) 302-7777", Email: "repoman@alwaysintense.com", FirstName: "Otto", LastName: "Maddox"}
+	// Creates a new instance of the appUser.User struct type
+	inputUsr := appUser.User{Username: "repoMan",
+		MobileID:  "(617) 302-7777",
+		Email:     "repoman@alwaysintense.com",
+		FirstName: "Otto",
+		LastName:  "Maddox"}
 
-	// //
-	logsWritten, tx, err := inputUsr.Create(ctx, env)
+	// Create method does validation and then inserts user into db
+	tx, err := inputUsr.Create(ctx, env)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("logsWritten = %d\n", logsWritten)
-
-	if logsWritten > 0 {
+	// Check to ensure that the CreateDate struct field is populated by
+	// making sure it's not at it's zero value to ensure that the db
+	// transaction was successful before commiting
+	if !inputUsr.CreateDate.IsZero() {
 		err = tx.Commit()
 		if err != nil {
 			log.Fatal(err)
 		}
-	} else if logsWritten <= 0 {
-		log.Fatal(err)
+	} else {
+		err = tx.Rollback()
+		log.Fatalln("CreateDate is nil, rolled back txn")
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
 	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
