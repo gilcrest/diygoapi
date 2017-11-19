f03cb9a9c4922cf1101aacd7480c6e9d0a2c16c9
Dan Gillis <gilcrest@gmail.com>
2017-11-19 07:03:41 -0800

    Added Adapt function

---
+++ pkg/api/server/handlers/dispatch.go
@@ -0,0 +1,25 @@
+package handlers
+
+import (
+	mwr "github.com/gilcrest/go-API-template/pkg/api/server/middleware"
+	"github.com/gilcrest/go-API-template/pkg/env"
+	"github.com/gorilla/mux"
+)
+
+// Dispatch is a way of organizing routing to handlers (versioning as well)
+func Dispatch(env *env.Env, rtr *mux.Router) *mux.Router {
+
+	// match only POST requests on /api/appUser/create
+	// This is the original (v1) version for the API and the response for this will never change
+	//  with versioning in order to maintain a stable contract
+	rtr.Handle("/api/appUser", mwr.Adapt(Handler{env, CreateUserHandler}, mwr.LogRequest(env))).
+		Methods("POST").
+		Headers("Content-Type", "application/json")
+
+	// match only POST requests on /api/v1/appUser/create
+	rtr.Handle("/api/v1/appUser", mwr.Adapt(Handler{env, CreateUserHandler}, mwr.LogRequest(env))).
+		Methods("POST").
+		Headers("Content-Type", "application/json")
+
+	return rtr
+}
