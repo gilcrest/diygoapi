c347f04f2b0dcb6f30ea43adff2a2f7e93067c53
Dan Gillis <gilcrest@gmail.com>
2017-11-24 15:09:39 -0800

    Added call to NewSubrouter

--- cmd/appUser/create/server/main.go
+++ cmd/appUser/create/server/main.go
@@ -22,14 +22,18 @@ func main() {
 	// create a new mux (multiplex) router
 	rtr := mux.NewRouter()
 
+	// send Router through subRouter function to add any standard
+	// Subroutes you may want for your APIs
+	r := handlers.NewSubrouter(rtr)
+
 	// API may have multiple versions and the matching may get a bit
 	// lengthy, this RouteMatch function helps with organizing that
-	rtr = handlers.Dispatch(env, rtr)
+	r = handlers.Dispatch(env, r)
 
 	// handle all requests with the Gorilla router by adding
 	// rtr to the DefaultServeMux
 	// LogRequest middleware will log request as well
-	http.Handle("/", rtr)
+	http.Handle("/", r)
 
 	if err := http.ListenAndServe("127.0.0.1:8080", nil); err != nil {
 		log.Fatal(err)
