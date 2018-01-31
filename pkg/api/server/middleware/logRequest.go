6dfdbb6d05d9287c2bffcfe1fa98a1155473e718
Dan Gillis <gilcrest@gmail.com>
2018-01-30 22:23:37 -0500

    re-org

--- pkg/api/server/middleware/logRequest.go
+++ pkg/api/server/middleware/logRequest.go
@@ -8,7 +8,6 @@ import (
 	"io/ioutil"
 	"net"
 	"net/http"
-	"net/http/httptest"
 	"net/http/httputil"
 	"strings"
 	"time"
@@ -18,17 +17,6 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-// const (
-// 	dumpReq   = "dumpRequest"
-// 	logReq    = "logRequest"
-// 	logResp   = "logResponse"
-// 	logReq2db = "logReq2db"
-// 	// m["reqwbody2db"] = true
-// 	// m["resp2db"] = true
-// 	// m["respwbody2db"] = true
-// 	// m["limited2db"] = true
-// )
-
 // APIAudit struct holds the http request and other attributes needed
 // for auditing an http request
 type APIAudit struct {
@@ -36,7 +24,7 @@ type APIAudit struct {
 	RequestID    string        `json:"request_id"`
 	TimeStarted  time.Time     `json:"time_started"`
 	TimeFinished time.Time     `json:"time_finished"`
-	TimeInMillis time.Duration `json:"time_in_millis"`
+	Duration     time.Duration `json:"time_in_millis"`
 	ResponseCode int           `json:"response_code"`
 	request
 	response request
@@ -88,15 +76,15 @@ func logReqDispatch(env *env.Env, aud *APIAudit, req *http.Request) error {
 
 	var err error
 
-	err = SetRequest(aud, req)
+	err = setRequest(aud, req)
 	if err != nil {
 		return err
 	}
 
 	if env.LogOpts.DumpRequest.Write {
-		// func DumpRequest(req *http.Request, body bool) ([]byte, error)
 		requestDump, err := httputil.DumpRequest(req, env.LogOpts.DumpRequest.Body)
 		if err != nil {
+			log.Error().Err(err).Msg("")
 			return err
 		}
 		log.Print(string(requestDump))
@@ -104,57 +92,19 @@ func logReqDispatch(env *env.Env, aud *APIAudit, req *http.Request) error {
 	}
 
 	if env.LogOpts.Log2StdOut.Request.Write {
-		err = logRequest(env, aud)
+		err = logReq2Stdout(env, aud)
 		if err != nil {
+			log.Error().Err(err).Msg("")
 			return err
 		}
 	}
 
-	// if logSwitch(env, logReq2db) {
-	// 	err = logRequest2Db(env, aud)
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// }
-
 	return nil
-
 }
 
-// func logSwitch(env *env.Env, logTest string) bool {
-
-// 	// If nothing is in the LogMap, then return false
-// 	if len(env.LogMap) == 0 {
-// 		return false
-// 	}
-
-// 	// for dumprequest, validate if true in map
-// 	if logTest == dumpReq {
-// 		if mv := env.LogMap[dumpReq]; mv {
-// 			return true
-// 		}
-// 	}
-
-// 	// for logRequest, validate if true in map
-// 	if logTest == logReq {
-// 		if mv := env.LogMap[logReq]; mv {
-// 			return true
-// 		}
-// 	}
-
-// 	// for logResponse, validate if true in map
-// 	if logTest == logResp {
-// 		if mv := env.LogMap[logResp]; mv {
-// 			return true
-// 		}
-// 	}
-
-// 	return false
-// }
-
-// SetRequest populates several core fields (TimeStarted, ctx and RequestID)
+// setRequest populates several core fields (TimeStarted, ctx and RequestID)
 // for the APIAudit struct being passed into the function
-func SetRequest(aud *APIAudit, req *http.Request) error {
+func setRequest(aud *APIAudit, req *http.Request) error {
 
 	var (
 		scheme string
@@ -163,6 +113,7 @@ func SetRequest(aud *APIAudit, req *http.Request) error {
 	// split host and port out for cleaner logging
 	host, port, err := net.SplitHostPort(req.Host)
 	if err != nil {
+		log.Error().Err(err).Msg("")
 		return err
 	}
 
@@ -178,12 +129,14 @@ func SetRequest(aud *APIAudit, req *http.Request) error {
 	// convert the Header map from the request to a JSON string
 	headerJSON, err := convertHeader(req.Header)
 	if err != nil {
+		log.Error().Err(err).Msg("")
 		return err
 	}
 
 	// convert the Trailer map from the request to a JSON string
 	trailerJSON, err := convertHeader(req.Trailer)
 	if err != nil {
+		log.Error().Err(err).Msg("")
 		return err
 	}
 
@@ -195,6 +148,7 @@ func SetRequest(aud *APIAudit, req *http.Request) error {
 
 	body, err := dumpBody(req)
 	if err != nil {
+		log.Error().Err(err).Msg("")
 		return err
 	}
 
@@ -218,32 +172,6 @@ func SetRequest(aud *APIAudit, req *http.Request) error {
 	aud.request.RequestURI = req.RequestURI
 
 	return nil
-
-}
-
-// SetResponse sets the response elements of the APIAudit payload
-func SetResponse(aud *APIAudit, rec *httptest.ResponseRecorder) error {
-	// set ResponseCode from ResponseRecorder
-	aud.ResponseCode = rec.Code
-
-	// set Header JSON from Header map in ResponseRecorder
-	headerJSON, err := convertHeader(rec.HeaderMap)
-	if err != nil {
-		return err
-	}
-	aud.response.Header = headerJSON
-
-	// Dump body to text using dumpBody function - need an http request
-	// struct, so use httptest.NewRequest to get one
-	req := httptest.NewRequest("POST", "http://example.com/foo", rec.Body)
-
-	body, err := dumpBody(req)
-	if err != nil {
-		return err
-	}
-	aud.response.Body = body
-
-	return nil
 }
 
 // convertHeader returns a JSON string representation of an http.Header map
@@ -251,6 +179,7 @@ func convertHeader(hdr http.Header) (string, error) {
 	// convert the http.Header map from the request to a slice of bytes
 	jsonBytes, err := json.Marshal(hdr)
 	if err != nil {
+		log.Error().Err(err).Msg("")
 		return "", err
 	}
 
@@ -340,7 +269,7 @@ func dumpBody(req *http.Request) (string, error) {
 // 	return lgr, nil
 // }
 
-func logRequest(env *env.Env, aud *APIAudit) error {
+func logReq2Stdout(env *env.Env, aud *APIAudit) error {
 
 	//var err error
 
@@ -381,143 +310,6 @@ func logRequest(env *env.Env, aud *APIAudit) error {
 		Str("RemoteAddr", aud.request.RemoteAddr).
 		Str("RequestURI", aud.request.RequestURI).
 		Msg("Request received")
-
-	return nil
-}
-
-// Creates a record in the appUser table using a stored function
-func logRequest2Db(env *env.Env, aud *APIAudit) error {
-
-	var (
-		rowsInserted int
-		respHdr      interface{}
-		reqHdr       interface{}
-	)
-
-	respHdr = headerNil(aud.response.Header)
-	reqHdr = headerNil(aud.request.Header)
-
-	// Calls the BeginTx method of the MainDb opened database
-	tx, err := env.DS.MainDb.BeginTx(aud.ctx, nil)
-	if err != nil {
-		return err
-	}
-
-	// Prepare the sql statement using bind variables
-	stmt, err := tx.PrepareContext(aud.ctx, `select api.log_request
-		(
-		p_request_id => $1,
-		p_request_timestamp => $2,
-		p_response_code => $3,
-		p_response_timestamp => $4,
-		p_duration_in_millis => $5,
-		p_protocol => $6,
-		p_protocol_major => $7,
-		p_protocol_minor => $8,
-		p_request_method => $9,
-		p_scheme => $10,
-		p_host => $11,
-		p_port => $12,
-		p_path => $13,
-		p_remote_address => $14,
-		p_request_header => $15,
-		p_request_content_length => $16,
-		p_request_body => $17,
-		p_response_header => $18,
-		p_response_body => $19)`)
-
-	if err != nil {
-		return err
-	}
-	defer stmt.Close()
-
-	rows, err := stmt.QueryContext(aud.ctx,
-		aud.RequestID,          //$1
-		aud.TimeStarted,        //$2
-		aud.ResponseCode,       //$3
-		aud.TimeFinished,       //$4
-		aud.TimeInMillis,       //$5
-		aud.request.Proto,      //$6
-		aud.request.ProtoMajor, //$7
-		aud.request.ProtoMinor, //$8
-		aud.request.Method,     //$9
-		aud.request.Scheme,     //$10
-		aud.request.Host,       //$11
-		aud.request.Port,       //$12
-		aud.request.Path,       //$13
-		aud.request.RemoteAddr, //$14
-		reqHdr,                 //$15
-		aud.request.ContentLength, //$16
-		aud.request.Body,          //$17
-		respHdr,                   //$18
-		aud.response.Body)         //$19
-
-	if err != nil {
-		log.Print(err)
-		return err
-	}
-	defer rows.Close()
-
-	// Iterate through the returned record(s)
-	for rows.Next() {
-		if err := rows.Scan(&rowsInserted); err != nil {
-			log.Print(err)
-			return err
-		}
-	}
-
-	err = rows.Err()
-	if err != nil {
-		return err
-	}
-
-	// If we have successfully written rows to the db
-	// we commit the transaction
-	err = tx.Commit()
-	if err != nil {
-		return err
-	}
-
-	return nil
-
-}
-
-// headerNil checks if the header field is an empty string
-// (the empty value for the string type) and switches it to
-// a nil.  An empty string is not allowed to be passed to a
-// JSONB type in postgres, however, a nil works
-func headerNil(hdr string) interface{} {
-	var respHdr interface{}
-
-	if hdr == "" {
-		respHdr = nil
-	} else {
-		respHdr = hdr
-	}
-
-	return respHdr
-}
-
-func logResponse(env *env.Env, aud *APIAudit) error {
-
-	//var err error
-
-	logger := env.Logger
-
-	logger.Debug().Msg("logResponse started")
-	defer logger.Debug().Msg("logResponse ended")
-
-	// logger, err = logFormValues(logger, req)
-	// if err != nil {
-	// 	return err
-	// }
-
-	logger.Info().
-		Str("request_id", aud.RequestID).
-		Int("response_code", aud.ResponseCode).
-		Str("header_json", aud.response.Header).
-		Str("body", aud.response.Body).
-		Msg("Response Sent")
 
 	return nil
 }
