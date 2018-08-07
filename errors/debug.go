579a84f9853eb6d0d18b4eec24f2a7657cf02f15
Dan Gillis <gilcrest@gmail.com>
2018-08-06 21:20:44 -0400

    Tweaks of errors package from "The Upspin Authors"

---
+++ errors/debug.go
@@ -0,0 +1,126 @@
+// Copyright 2016 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build debug
+
+package errors
+
+import (
+	"bytes"
+	"fmt"
+	"runtime"
+	"strings"
+)
+
+// stack is a type that is embedded in an Error struct, and contains
+// information about the stack that created that Error.
+type stack struct {
+	callers []uintptr
+	// TODO(adg): add time of creation
+}
+
+// populateStack uses the runtime to populate the Error's stack struct with
+// information about the current stack. It should be called from the E
+// function, when the Error is being created.
+// If the Error has another Error value in its Err field, populateStack
+// coalesces the stack from the inner error (if any) with the current stack,
+// so that any given Error value only prints one stack.
+func (e *Error) populateStack() {
+	e.callers = callers()
+	e2, ok := e.Err.(*Error)
+	if !ok {
+		return
+	}
+	// Move distinct callers from inner error to outer error
+	// (and throw the common callers away)
+	// so that we only print the stack trace once.
+	i := 0
+	ok = false
+	for ; i < len(e.callers) && i < len(e2.callers); i++ {
+		if e.callers[len(e.callers)-1-i] != e2.callers[len(e2.callers)-1-i] {
+			break
+		}
+		ok = true
+	}
+	if ok { // The stacks have some PCs in common.
+		head := e2.callers[:len(e2.callers)-i]
+		tail := e.callers
+		e.callers = make([]uintptr, len(head)+len(tail))
+		copy(e.callers, head)
+		copy(e.callers[len(head):], tail)
+		e2.callers = nil
+	}
+}
+
+// frame returns the nth frame, with the frame at top of stack being 0.
+func frame(callers []uintptr, n int) *runtime.Frame {
+	frames := runtime.CallersFrames(callers)
+	var f runtime.Frame
+	for i := len(callers) - 1; i >= n; i-- {
+		var ok bool
+		f, ok = frames.Next()
+		if !ok {
+			break // Should never happen, and this is just debugging.
+		}
+	}
+	return &f
+}
+
+// printStack formats and prints the stack for this Error to the given buffer.
+// It should be called from the Error's Error method.
+func (e *Error) printStack(b *bytes.Buffer) {
+	printCallers := callers()
+	// Iterate backward through e.callers (the last in the stack is the
+	// earliest call, such as main) skipping over the PCs that are shared
+	// by the error stack and by this function call stack, printing the
+	// names of the functions and their file names and line numbers.
+	var prev string // the name of the last-seen function
+	var diff bool   // do the print and error call stacks differ now?
+	for i := 0; i < len(e.callers); i++ {
+		thisFrame := frame(e.callers, i)
+		name := thisFrame.Func.Name()
+		if !diff && i < len(printCallers) {
+			if name == frame(printCallers, i).Func.Name() {
+				// both stacks share this PC, skip it.
+				continue
+			}
+			// No match, don't consider printCallers again.
+			diff = true
+		}
+		// Don't print the same function twice.
+		// (Can happen when multiple error stacks have been coalesced.)
+		if name == prev {
+			continue
+		}
+		// Find the uncommon prefix between this and the previous
+		// function name, separating by dots and slashes.
+		trim := 0
+		for {
+			j := strings.IndexAny(name[trim:], "./")
+			if j < 0 {
+				break
+			}
+			if !strings.HasPrefix(prev, name[:j+trim]) {
+				break
+			}
+			trim += j + 1 // skip over the separator
+		}
+		// Do the printing.
+		pad(b, Separator)
+		fmt.Fprintf(b, "%v:%d: ", thisFrame.File, thisFrame.Line)
+		if trim > 0 {
+			b.WriteString("...")
+		}
+		b.WriteString(name[trim:])
+		prev = name
+	}
+}
+
+// callers is a wrapper for runtime.Callers that allocates a slice.
+func callers() []uintptr {
+	var stk [64]uintptr
+	const skip = 4 // Skip 4 stack frames; ok for both E and Error funcs.
+	n := runtime.Callers(skip, stk[:])
+	return stk[:n]
+}
---
+++ errors/errors_test.go
@@ -0,0 +1,201 @@
+// Copyright 2016 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build !debug
+
+package errors
+
+import (
+	"io"
+	"os"
+	"os/exec"
+	"testing"
+
+	"github.com/gilcrest/fide/common"
+)
+
+func TestDebug(t *testing.T) {
+	// Test with -tags debug to run the tests in debug_test.go
+	cmd := exec.Command("go", "test", "-tags", "debug")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		t.Fatalf("external go test failed: %v", err)
+	}
+}
+func TestMarshal(t *testing.T) {
+	path := PathName("jane@doe.com/file")
+	user := common.UserName("joe@blow.com")
+	// Single error. No user is set, so we will have a zero-length field inside.
+	e1 := E(Op("Get"), path, IO, "network unreachable")
+	// Nested error.
+	e2 := E(Op("Read"), path, user, Other, e1)
+	b := MarshalError(e2)
+	e3 := UnmarshalError(b)
+	in := e2.(*Error)
+	out := e3.(*Error)
+	// Compare elementwise.
+	if in.Path != out.Path {
+		t.Errorf("expected Path %q; got %q", in.Path, out.Path)
+	}
+	if in.User != out.User {
+		t.Errorf("expected User %q; got %q", in.User, out.User)
+	}
+	if in.Op != out.Op {
+		t.Errorf("expected Op %q; got %q", in.Op, out.Op)
+	}
+	if in.Kind != out.Kind {
+		t.Errorf("expected kind %d; got %d", in.Kind, out.Kind)
+	}
+	// Note that error will have lost type information, so just check its Error string.
+	if in.Err.Error() != out.Err.Error() {
+		t.Errorf("expected Err %q; got %q", in.Err, out.Err)
+	}
+}
+func TestSeparator(t *testing.T) {
+	defer func(prev string) {
+		Separator = prev
+	}(Separator)
+	Separator = ":: "
+	// Same pattern as above.
+	path := PathName("jane@doe.com/file")
+	user := common.UserName("joe@blow.com")
+	// Single error. No user is set, so we will have a zero-length field inside.
+	e1 := E(Op("Get"), path, IO, "network unreachable")
+	// Nested error.
+	e2 := E(Op("Read"), path, user, Other, e1)
+	want := "Read: jane@doe.com/file, user joe@blow.com: I/O error:: Get: network unreachable"
+	if errorAsString(e2) != want {
+		t.Errorf("expected %q; got %q", want, e2)
+	}
+}
+func TestDoesNotChangePreviousError(t *testing.T) {
+	err := E(Permission)
+	err2 := E(Op("I will NOT modify err"), err)
+	expected := "I will NOT modify err: permission denied"
+	if errorAsString(err2) != expected {
+		t.Fatalf("Expected %q, got %q", expected, err2)
+	}
+	kind := err.(*Error).Kind
+	if kind != Permission {
+		t.Fatalf("Expected kind %v, got %v", Permission, kind)
+	}
+}
+func TestNoArgs(t *testing.T) {
+	defer func() {
+		err := recover()
+		if err == nil {
+			t.Fatal("E() did not panic")
+		}
+	}()
+	_ = E()
+}
+
+type matchTest struct {
+	err1, err2 error
+	matched    bool
+}
+
+const (
+	path1 = PathName("john@doe.io/x")
+	path2 = PathName("john@doe.io/y")
+	john  = common.UserName("john@doe.io")
+	jane  = common.UserName("jane@doe.io")
+)
+const (
+	op  = Op("Op")
+	op1 = Op("Op1")
+	op2 = Op("Op2")
+)
+
+var matchTests = []matchTest{
+	// Errors not of type *Error fail outright.
+	{nil, nil, false},
+	{io.EOF, io.EOF, false},
+	{E(io.EOF), io.EOF, false},
+	{io.EOF, E(io.EOF), false},
+	// Success. We can drop fields from the first argument and still match.
+	{E(io.EOF), E(io.EOF), true},
+	{E(op, Invalid, io.EOF, jane, path1), E(op, Invalid, io.EOF, jane, path1), true},
+	{E(op, Invalid, io.EOF, jane), E(op, Invalid, io.EOF, jane, path1), true},
+	{E(op, Invalid, io.EOF), E(op, Invalid, io.EOF, jane, path1), true},
+	{E(op, Invalid), E(op, Invalid, io.EOF, jane, path1), true},
+	{E(op), E(op, Invalid, io.EOF, jane, path1), true},
+	// Failure.
+	{E(io.EOF), E(io.ErrClosedPipe), false},
+	{E(op1), E(op2), false},
+	{E(Invalid), E(Permission), false},
+	{E(jane), E(john), false},
+	{E(path1), E(path2), false},
+	{E(op, Invalid, io.EOF, jane, path1), E(op, Invalid, io.EOF, john, path1), false},
+	{E(path1, Str("something")), E(path1), false}, // Test nil error on rhs.
+	// Nested *Errors.
+	{E(op1, E(path1)), E(op1, john, E(op2, jane, path1)), true},
+	{E(op1, path1), E(op1, john, E(op2, jane, path1)), false},
+	{E(op1, E(path1)), E(op1, john, Str(E(op2, jane, path1).Error())), false},
+}
+
+func TestMatch(t *testing.T) {
+	for _, test := range matchTests {
+		matched := Match(test.err1, test.err2)
+		if matched != test.matched {
+			t.Errorf("Match(%q, %q)=%t; want %t", test.err1, test.err2, matched, test.matched)
+		}
+	}
+}
+
+type kindTest struct {
+	err  error
+	kind Kind
+	want bool
+}
+
+var kindTests = []kindTest{
+	// Non-Error errors.
+	{nil, NotExist, false},
+	{Str("not an *Error"), NotExist, false},
+	// Basic comparisons.
+	{E(NotExist), NotExist, true},
+	{E(Exist), NotExist, false},
+	{E("no kind"), NotExist, false},
+	{E("no kind"), Other, false},
+	// Nested *Error values.
+	{E("Nesting", E(NotExist)), NotExist, true},
+	{E("Nesting", E(Exist)), NotExist, false},
+	{E("Nesting", E("no kind")), NotExist, false},
+	{E("Nesting", E("no kind")), Other, false},
+}
+
+func TestKind(t *testing.T) {
+	for _, test := range kindTests {
+		got := Is(test.kind, test.err)
+		if got != test.want {
+			t.Errorf("Is(%q, %q)=%t; want %t", test.kind, test.err, got, test.want)
+		}
+	}
+}
+
+// errorAsString returns the string form of the provided error value.
+// If the given string is an *Error, the stack information is removed
+// before the value is stringified.
+func errorAsString(err error) string {
+	if e, ok := err.(*Error); ok {
+		e2 := *e
+		e2.stack = stack{}
+		return e2.Error()
+	}
+	return err.Error()
+}
+
+// Simple test for issue 398.
+func TestIssue398(t *testing.T) {
+	e := E("a@b.com", "c@d.com", "e@f.com/", "g@h.com/").(*Error)
+	// First should win.
+	if e.User != "a@b.com" {
+		t.Errorf("wrong user: got %q; want %q", e.User, "a@b.com")
+	}
+	if e.Path != "e@f.com/" {
+		t.Errorf("wrong path:  got %q; want %q", e.Path, "e@f.com/")
+	}
+}
