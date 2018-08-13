68eaf90d4e5f68ca20dee9d99aa308b1599b2f56
Dan Gillis <gilcrest@gmail.com>
2018-08-13 14:51:36 -0700

    error handling

--- server/handler/login.go
+++ server/handler/login.go
@@ -2,34 +2,70 @@ package handler
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 
 	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gilcrest/go-API-template/auth"
+	"github.com/gilcrest/go-API-template/db"
 	"github.com/gilcrest/go-API-template/env"
+	"github.com/gilcrest/go-API-template/server/errorHandler"
 )
 
-// LoginHandler is for member login
-// TODO better description
+// LoginHandler is for user login
 func LoginHandler(env *env.Env, w http.ResponseWriter, req *http.Request) error {
+
+	// Get a new logger instance
+	log := env.Logger
+
+	log.Debug().Msg("Start handler.LoginHandler")
+	defer log.Debug().Msg("Finish handler.LoginHandler")
 
 	// retrieve the context from the http.Request
 	ctx := req.Context()
 
-	// Fire up the db txns (MainDb and Logger DB)
-	err := env.DS.SetTx(ctx, nil)
+	// Begin a new LogDB txn
+	_, err := env.DS.BeginTx(ctx, nil, db.LogDB)
 	if err != nil {
 		return err
 	}
 
+	// Declare creds as an instance of auth.Credentials
+	// Decode JSON HTTP request body into a Decoder type
+	//  and unmarshal that into creds
 	creds := new(auth.Credentials)
-
-	_ = json.NewDecoder(req.Body).Decode(&creds)
-
-	usr, err := auth.Authorise(ctx, env, creds)
+	err = json.NewDecoder(req.Body).Decode(&creds)
 	if err != nil {
-		return err
+		return errorHandler.HTTPErr{
+			Code: http.StatusBadRequest,
+			Type: "invalid_request",
+			Err:  err,
+		}
+	}
+	defer req.Body.Close()
+
+	tx, err := env.DS.BeginTx(ctx, nil, db.AppDB)
+	if err != nil {
+		return errorHandler.HTTPErr{
+			Code: http.StatusBadRequest,
+			Type: "database_error",
+			Err:  err,
+		}
+	}
+
+	// Pass user credentials to auth.Authorise
+	// If an error is passed back, send a generic error so as not
+	// to allow end user to know if it was the username or password
+	// we can tell via logs what it was, but don't want end user to
+	// know (bad security practice)
+	usr, err := auth.Authorize(ctx, log, tx, creds)
+	if err != nil {
+		return errorHandler.HTTPErr{
+			Code: http.StatusUnauthorized,
+			Type: "unauthorised",
+			Err:  errors.New("Wrong email or password"),
+		}
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
