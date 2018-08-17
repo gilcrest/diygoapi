422c95928fa653a4b0c49a833bd9b9ce70bbb1c0
Dan Gillis <gilcrest@gmail.com>
2018-08-17 08:53:56 -0700

    setting RequestID to context

--- auth/auth.go
+++ auth/auth.go
@@ -4,12 +4,22 @@ import (
 	"context"
 	"database/sql"
 
+	"github.com/rs/xid"
 	"github.com/rs/zerolog"
 
 	"github.com/gilcrest/go-API-template/appuser"
 	"github.com/gilcrest/go-API-template/errors"
 	"golang.org/x/crypto/bcrypt"
 )
+
+type contextKey string
+
+func (c contextKey) String() string {
+	return "context key " + string(c)
+}
+
+// RequestID is a unique identifier for each inbound request
+var requestID = contextKey("RequestID")
 
 // ErrPassNotMatch is an error when a given password hash
 // does not match the password hash in the database
@@ -49,4 +59,23 @@ func Authorize(ctx context.Context, log zerolog.Logger, tx *sql.Tx, c *Credentia
 func checkPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
+}
+
+func setRequestID(ctx context.Context) context.Context {
+	// get byte Array representation of guid from xid package (12 bytes)
+	guid := xid.New()
+
+	// use the String method of the guid object to convert byte array to string (20 bytes)
+	rID := guid.String()
+
+	ctx = context.WithValue(ctx, requestID, rID)
+
+	return ctx
+
+}
+
+// RequestID gets the Request ID from the context.
+func RequestID(ctx context.Context) (string, bool) {
+	requestIDstr, ok := ctx.Value(requestID).(string)
+	return requestIDstr, ok
 }
