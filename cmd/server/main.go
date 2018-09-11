9e7e8719a902d804f09defa09f4a02aab1792ee5
Dan Gillis <gilcrest@gmail.com>
2018-09-10 23:14:39 -0400

    Flags for log level

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 
@@ -10,7 +11,13 @@ import (
 
 func main() {
 
-	srv, err := app.NewServer(zerolog.DebugLevel)
+	loglvlFlag := flag.String("loglvl", "error", "sets log level")
+
+	flag.Parse()
+
+	loglevel := logLevel(loglvlFlag)
+
+	srv, err := app.NewServer(loglevel)
 	if err != nil {
 		panic(err)
 	}
@@ -23,4 +30,30 @@ func main() {
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func logLevel(s *string) zerolog.Level {
+
+	var lvl zerolog.Level
+
+	// dereference the string pointer to get flag value
+	ds := *s
+
+	switch ds {
+	case "debug":
+		lvl = zerolog.DebugLevel
+	case "info":
+		lvl = zerolog.InfoLevel
+	case "warn":
+		lvl = zerolog.WarnLevel
+	case "fatal":
+		lvl = zerolog.FatalLevel
+	case "panic":
+		lvl = zerolog.PanicLevel
+	case "disabled":
+		lvl = zerolog.Disabled
+	default:
+		lvl = zerolog.ErrorLevel
+	}
+	return lvl
 }
