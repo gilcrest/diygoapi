814ee207f093478dbb1e835d63badc45d2bab5b8
Dan Gillis <gilcrest@gmail.com>
2018-09-28 22:06:42 -0400

    Removed newOpts call

--- app/routes.go
+++ app/routes.go
@@ -12,7 +12,7 @@ func (s *server) routes() error {
 	// Get a logger instance from the server struct
 	log := s.logger
 
-	// Get logging Database to pass into httplog
+	// Get pointer to logging database to pass into httplog
 	// Only need this if you plan to use the PostgreSQL
 	// logging style of httplog
 	logdb, err := s.ds.DB(datastore.LogDB)
@@ -20,20 +20,40 @@ func (s *server) routes() error {
 		return err
 	}
 
-	s.router.HandleFunc("/v1/handlefunc/user",
-		httplog.LogHandlerFunc(s.handleUserCreate(), log, logdb, nil))
+	// httplog.NewOpts gets a new httplog.Opts struct
+	// (with all flags set to false)
+	opts := new(httplog.Opts)
 
+	// For the examples below, I chose to turn on db logging only
+	// Log the request headers only (body has password on this api!)
+	// Log both the response headers and body
+	opts.Log2DB.Enable = true
+	opts.Log2DB.Request.Header = true
+	opts.Log2DB.Response.Header = true
+	opts.Log2DB.Response.Body = true
+
+	// HandlerFunc middleware example
+	// function takes an http.HandlerFunc and returns an http.HandlerFunc
+	// Also, match only POST requests with Content-Type header = application/json
+	s.router.HandleFunc("/v1/handlefunc/user",
+		httplog.LogHandlerFunc(s.handleUserCreate(), log, logdb, opts)).
+		Methods("POST").
+		Headers("Content-Type", "application/json")
+
+	// function (`LogHandler`) that takes a handler and returns a handler (aka Constructor)
+	// (`func (http.Handler) http.Handler`)	- used with alice
+	// Also, match only POST requests with Content-Type header = application/json
 	s.router.Handle("/v1/alice/user",
-		alice.New(httplog.LogHandler(log, logdb, nil)).
+		alice.New(httplog.LogHandler(log, logdb, opts)).
 			ThenFunc(s.handleUserCreate())).
 		Methods("POST").
 		Headers("Content-Type", "application/json")
 
-	// match only POST requests on /v1/adapter/user
-	// having a Content-Type header = application/json
+	// Adapter Type middleware example
+	// Also, match only POST requests with Content-Type header = application/json
 	s.router.Handle("/v1/adapter/user",
 		httplog.Adapt(s.handleUserCreate(),
-			httplog.LogAdapter(log, logdb, nil))).
+			httplog.LogAdapter(log, logdb, opts))).
 		Methods("POST").
 		Headers("Content-Type", "application/json")
 
