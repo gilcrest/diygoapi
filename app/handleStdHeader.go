a4d27e4f4842a169c0dbed6549858830a72031c7
Dan Gillis <gilcrest@gmail.com>
2018-11-29 14:49:21 -0500

    Starting to add auth

--- app/handleStdHeader.go
+++ app/handleStdHeader.go
@@ -1,12 +1,39 @@
 package app
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gilcrest/errors"
+)
 
 // handleStdHeader middleware is used to add standard HTTP response headers
 func (s *server) handleStdHeader(h http.Handler) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
+			fmt.Println("Got into handleStdHeader")
 			w.Header().Add("Content-Type", "application/json")
+			h.ServeHTTP(w, r) // call original
+		})
+}
+
+func (s *server) handleAuth(h http.Handler) http.Handler {
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+
+			fmt.Println(w.Header())
+
+			err := errors.Str("fake")
+
+			if err == nil {
+				err = errors.HTTPErr{
+					Code: http.StatusUnauthorized,
+					Kind: errors.Permission,
+					Err:  err,
+				}
+				errors.HTTPError(w, err)
+				return
+			}
 			h.ServeHTTP(w, r) // call original
 		})
 }
