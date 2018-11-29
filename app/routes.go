07034d3ebc4de035dd6536dff0f71cc056947789
Dan Gillis <gilcrest@gmail.com>
2018-11-29 14:49:34 -0500

    auth changes

--- app/routes.go
+++ app/routes.go
@@ -44,7 +44,7 @@ func (s *server) routes() error {
 	// (`func (http.Handler) http.Handler`)	- used with alice
 	// Also, match only POST requests with Content-Type header = application/json
 	s.router.Handle("/v1/alice/user",
-		alice.New(httplog.LogHandler(log, logdb, opts), s.handleStdHeader).
+		alice.New(httplog.LogHandler(log, logdb, opts), s.handleStdHeader, s.handleAuth).
 			ThenFunc(s.handleUserCreate())).
 		Methods("POST").
 		Headers("Content-Type", "application/json")
