c0186212fbe492f6bf386b5da551ae8368591cd8
Dan Gillis <gilcrest@gmail.com>
2019-11-15 14:22:03 -0500

    reorg datastore package

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -3,6 +3,9 @@ package datastore
 import (
 	"context"
 	"database/sql"
+	"fmt"
+	"os"
+	"strconv"
 
 	"errors"
 
@@ -115,4 +118,131 @@ func ProvideDatastore(n DSName) (Datastore, error) {
 	}
 
 	return &DS{DB: db}, nil
+}
+
+// provideDB returns an open database handle of 0 or more underlying connections
+func provideDB(n DSName) (*sql.DB, error) {
+	const op errs.Op = "datastore/provideDB"
+
+	// If we are in "mock mode", we return a nil database
+	if n == MockDatastore {
+		return nil, nil
+	}
+
+	// Get Database connection credentials from environment variables
+	dbNme := os.Getenv(dbEnvName(n))
+	dbUser := os.Getenv(dbEnvUser(n))
+	dbPassword := os.Getenv(dbEnvPassword(n))
+	dbHost := os.Getenv(dbEnvHost(n))
+	dbPort, err := strconv.Atoi(os.Getenv(dbEnvPort(n)))
+	if err != nil {
+		return nil, errs.E(op, err)
+	}
+
+	// Craft string for database connection
+	dbinfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbPassword, dbNme)
+
+	// Open the postgres database using the postgres driver (pq)
+	// func Open(driverName, dataSourceName string) (*DB, error)
+	db, err := sql.Open("postgres", dbinfo)
+	if err != nil {
+		return nil, errs.E(op, err)
+	}
+
+	// Call Ping to validate the newly opened database is actually alive
+	if err = db.Ping(); err != nil {
+		return nil, errs.E(op, err)
+	}
+	return db, nil
+}
+
+func dbEnvName(n DSName) string {
+	switch n {
+	case AppDatastore:
+		return envAppDBName
+	case LogDatastore:
+		return envLogDBName
+	default:
+		return ""
+	}
+}
+
+func dbEnvUser(n DSName) string {
+	switch n {
+	case AppDatastore:
+		return envAppDBUser
+	case LogDatastore:
+		return envLogDBUser
+	default:
+		return ""
+	}
+}
+
+func dbEnvPassword(n DSName) string {
+	switch n {
+	case AppDatastore:
+		return envAppDBPassword
+	case LogDatastore:
+		return envLogDBPassword
+	default:
+		return ""
+	}
+}
+
+func dbEnvHost(n DSName) string {
+	switch n {
+	case AppDatastore:
+		return envAppDBHost
+	case LogDatastore:
+		return envLogDBHost
+	default:
+		return ""
+	}
+}
+
+func dbEnvPort(n DSName) string {
+	switch n {
+	case AppDatastore:
+		return envAppDBPort
+	case LogDatastore:
+		return envLogDBPort
+	default:
+		return ""
+	}
+}
+
+// MockDS is a mock implementation for a database
+type MockDS struct {
+}
+
+// BeginTx is a wrapper for sql.DB.BeginTx in order to expose from
+// the Datastore interface
+func (db *MockDS) BeginTx(ctx context.Context) error {
+	const op errs.Op = "datastore/MockDS.BeginTx"
+
+	return nil
+}
+
+// Tx exposes the Tx stored in the struct in order to be exposed from
+// the Datastore interface.
+func (db *MockDS) Tx() (*sql.Tx, error) {
+	const op errs.Op = "datastore/MockDS.Tx"
+
+	return nil, nil
+}
+
+// RollbackTx is a wrapper for sql.Tx.Rollback in order to expose from
+// the Datastore interface. Proper error handling is also considered.
+func (db *MockDS) RollbackTx(err error) error {
+	const op errs.Op = "datastore/MockDS.RollbackTx"
+
+	return errs.E(op, errs.Database, err)
+}
+
+// CommitTx is a wrapper for sql.Tx.Commit in order to expose from
+// the Datastore interface. Proper error handling is also considered.
+func (db *MockDS) CommitTx() error {
+	const op errs.Op = "datastore/MockDS.CommitTx"
+
+	return nil
 }
