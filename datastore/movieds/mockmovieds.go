8fe65a9faaa59f7ecd7dd6bb4896fe1c90359190
Dan Gillis <gilcrest@gmail.com>
2019-11-26 11:26:40 -0500

    Updated mocks

--- datastore/movieds/mockmovieds.go
+++ datastore/movieds/mockmovieds.go
@@ -26,10 +26,47 @@ func (mdb MockMovieDB) Store(ctx context.Context, m *movie.Movie) error {
 func (mdb MockMovieDB) FindByID(ctx context.Context, extlID xid.ID) (*movie.Movie, error) {
 	const op errs.Op = "movieds/MockMovieDB.FindByID"
 
-	m := new(movie.Movie)
-	m.ExtlID = extlID
-	m.Title = "Clockwork Orange"
-	m.CreateTimestamp = time.Now()
+	m1 := new(movie.Movie)
+	m1.ExtlID = xid.New()
+	m1.Title = "The Thing"
+	m1.Year = 1982
+	m1.Rated = "R"
+	m1.Released = time.Date(1982, time.June, 25, 0, 0, 0, 0, time.UTC)
+	m1.RunTime = 109
+	m1.Director = "John Carpenter"
+	m1.Writer = "Bill Lancaster"
+	m1.CreateTimestamp = time.Now()
 
-	return m, nil
+	return m1, nil
+}
+
+// FindAll returns a slice of Movie structs to populate the response
+func (mdb MockMovieDB) FindAll(ctx context.Context) ([]*movie.Movie, error) {
+	const op errs.Op = "movieds/MockMovieDB.FindAll"
+
+	m1 := new(movie.Movie)
+	m1.ExtlID = xid.New()
+	m1.Title = "The Thing"
+	m1.Year = 1982
+	m1.Rated = "R"
+	m1.Released = time.Date(1982, time.June, 25, 0, 0, 0, 0, time.UTC)
+	m1.RunTime = 109
+	m1.Director = "John Carpenter"
+	m1.Writer = "Bill Lancaster"
+	m1.CreateTimestamp = time.Now()
+
+	m2 := new(movie.Movie)
+	m2.ExtlID = xid.New()
+	m2.Title = "Repo Man"
+	m2.Year = 1984
+	m2.Rated = "R"
+	m2.Released = time.Date(1984, time.March, 2, 0, 0, 0, 0, time.UTC)
+	m2.RunTime = 109
+	m2.Director = "Alex Cox"
+	m2.Writer = "Alex Cox"
+	m2.CreateTimestamp = time.Now()
+
+	s := []*movie.Movie{m1, m2}
+
+	return s, nil
 }
