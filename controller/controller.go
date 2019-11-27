543b5b92d9e9c63d49d518d61b4af2fa11850c26
Dan Gillis <gilcrest@gmail.com>
2019-11-26 23:49:16 -0500

    Add RequestID

--- controller/controller.go
+++ controller/controller.go
@@ -7,19 +7,29 @@ import (
 	"github.com/rs/xid"
 )
 
+// RequestID is the unique Request ID for each request
+type RequestID struct {
+	xid.ID
+}
+
 // StandardResponseFields is meant to be included in all response bodies
 // and includes "standard" response fields
 type StandardResponseFields struct {
-	Path      string `json:"path,omitempty"`
-	RequestID xid.ID `json:"request_id"`
+	Path string    `json:"path,omitempty"`
+	ID   RequestID `json:"request_id"`
+}
+
+// NewRequestID is an initializer for RequestID
+func NewRequestID(id xid.ID) RequestID {
+	return RequestID{ID: id}
 }
 
 // NewStandardResponseFields is an initializer for the StandardResponseFields struct
-func NewStandardResponseFields(id xid.ID, r *http.Request) *StandardResponseFields {
+func NewStandardResponseFields(id RequestID, r *http.Request) StandardResponseFields {
 	const op errs.Op = "controller/moviectl/NewStandardResponse"
 
-	sr := new(StandardResponseFields)
-	sr.RequestID = id
+	var sr StandardResponseFields
+	sr.ID = id
 	sr.Path = r.URL.EscapedPath()
 
 	return sr
