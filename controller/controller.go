abccf40a193cb2da6cb1e92561922e8edb0cb7f2
Dan Gillis <gilcrest@gmail.com>
2020-02-25 06:42:19 -0500

    Rename RequestID to TraceID

--- controller/controller.go
+++ controller/controller.go
@@ -1,32 +1,43 @@
 package controller
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/rs/xid"
 )
 
 // RequestID is the unique Request ID for each request
-type RequestID struct {
+type TraceID struct {
 	xid.ID
 }
 
 // StandardResponseFields is meant to be included in all response bodies
 // and includes "standard" response fields
 type StandardResponseFields struct {
-	Path string    `json:"path,omitempty"`
-	ID   RequestID `json:"request_id"`
+	Path    string  `json:"path,omitempty"`
+	TraceID TraceID `json:"trace_id,omitempty"`
 }
 
-// NewRequestID is an initializer for RequestID
-func NewRequestID(id xid.ID) RequestID {
-	return RequestID{ID: id}
+// NewTraceID is an initializer for TraceID
+func NewTraceID(id xid.ID) TraceID {
+	return TraceID{ID: id}
+}
+
+// NewMockTraceID is an initializer for TraceID which returns a
+// static "mocked"
+func NewMockTraceID() TraceID {
+	x, err := xid.FromString("bpa182jipt3b2b78879g")
+	if err != nil {
+		fmt.Println(err)
+	}
+	return TraceID{ID: x}
 }
 
 // NewStandardResponseFields is an initializer for the StandardResponseFields struct
-func NewStandardResponseFields(id RequestID, r *http.Request) StandardResponseFields {
+func NewStandardResponseFields(id TraceID, r *http.Request) StandardResponseFields {
 	var sr StandardResponseFields
-	sr.ID = id
+	sr.TraceID = id
 	sr.Path = r.URL.EscapedPath()
 
 	return sr
