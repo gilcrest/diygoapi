8a308a1e5cb270ed4334b032414799da863569a4
Dan Gillis <gilcrest@gmail.com>
2020-12-10 12:44:28 -0500

    move out parts to auth package

--- handler/accessTokenHandler.go
+++ handler/accessTokenHandler.go
@@ -1,44 +1,24 @@
 package handler
 
 import (
-	"context"
 	"net/http"
 	"strings"
 
-	"github.com/rs/zerolog/hlog"
+	"github.com/gilcrest/go-api-basic/domain/auth"
+	"github.com/gilcrest/go-api-basic/domain/errs"
 
 	"github.com/pkg/errors"
-
-	"github.com/gilcrest/go-api-basic/domain/errs"
+	"github.com/rs/zerolog/hlog"
 )
-
-type contextKey string
-
-const contextKeyAccessToken = contextKey("access-token")
-
-// accessToken gets the access token from the request
-func accessToken(r *http.Request) (string, error) {
-
-	// retrieve the context from the http.Request
-	ctx := r.Context()
-
-	tokenStr, ok := ctx.Value(contextKeyAccessToken).(string)
-	if !ok {
-		return "", errs.E(errs.Unauthenticated, errors.New("Access Token not set properly to context"))
-	}
-	if tokenStr == "" {
-		return "", errs.E(errs.Unauthenticated, errors.New("Access Token empty in context"))
-	}
-	return tokenStr, nil
-}
 
 // AccessTokenHandler middleware is used to set the Google access
 // token to the context
 func (ah *AppHandler) AccessTokenHandler(h http.Handler) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
-			logger := *hlog.FromRequest(r)
+			const tokenType string = "Bearer"
 
+			logger := *hlog.FromRequest(r)
 			var token string
 
 			// retrieve the context from the http.Request
@@ -51,7 +31,7 @@ func (ah *AppHandler) AccessTokenHandler(h http.Handler) http.Handler {
 			headerValue, ok := r.Header["Authorization"]
 			if ok && len(headerValue) >= 1 {
 				token = headerValue[0]
-				token = strings.TrimPrefix(token, "Bearer ")
+				token = strings.TrimPrefix(token, tokenType+" ")
 			}
 
 			// If the token is empty...
@@ -70,7 +50,7 @@ func (ah *AppHandler) AccessTokenHandler(h http.Handler) http.Handler {
 			}
 
 			// add access token to context
-			ctx = context.WithValue(ctx, contextKeyAccessToken, token)
+			ctx = auth.SetAccessToken2Context(ctx, token, tokenType)
 
 			// call original, adding access token to request context
 			h.ServeHTTP(w, r.WithContext(ctx))
