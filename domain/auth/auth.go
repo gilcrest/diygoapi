21b8cd3e40337354fb4ba0b4ee42f634e33eb26f
Dan Gillis <gilcrest@gmail.com>
2020-12-10 12:45:49 -0500

    refactor for access token and authorization

--- domain/auth/auth.go
+++ domain/auth/auth.go
@@ -3,19 +3,89 @@ package auth
 import (
 	"context"
 	"fmt"
+	"net/http"
 
-	"github.com/gilcrest/errs"
+	"github.com/rs/zerolog"
+
+	"golang.org/x/oauth2"
+	googleoauth "google.golang.org/api/oauth2/v2"
+
+	"github.com/pkg/errors"
+
+	"github.com/gilcrest/go-api-basic/domain/errs"
 	"github.com/gilcrest/go-api-basic/domain/user"
 )
 
-func AuthorizeUser(ctx context.Context, u *user.User) error {
-	const op errs.Op = "domain/auth/AuthorizeUser"
+// AccessToken represents an Oauth2 access token found
+// in an HTTP header as a Bearer token
+type AccessToken struct {
+	Token     string
+	TokenType string
+}
 
-	m := make(map[string]bool)
+// NewGoogleOauth2Token returns a Google Oauth2 token given an AccessToken
+func (at AccessToken) NewGoogleOauth2Token() *oauth2.Token {
+	return &oauth2.Token{AccessToken: at.Token, TokenType: at.TokenType}
+}
 
-	m["gilcrest@gmail.com"] = true
+// newUser initializes the user.User struct given a Userinfo struct
+// from Google
+func newUser(userinfo *googleoauth.Userinfo) *user.User {
+	return &user.User{
+		Email:     userinfo.Email,
+		LastName:  userinfo.FamilyName,
+		FirstName: userinfo.GivenName,
+		FullName:  userinfo.Name,
+		//Gender:       userinfo.Gender,
+		HostedDomain: userinfo.Hd,
+		PictureURL:   userinfo.Picture,
+		ProfileLink:  userinfo.Link,
+	}
+}
 
-	authorized, exists := m[u.Email]
+// Authorizer interface authorizes access to a resource given
+// a user and action
+type Authorizer interface {
+	Authorize(ctx context.Context, sub *user.User, obj string, act string) error
+}
+
+// Auth struct satisfies the Authorizer interface
+type Auth struct{}
+
+// Authorize authorizes a subject (user) can perform a particular
+// action on an object. e.g. gilcrest can read (GET) the resource
+// at the /ping path
+func (a Auth) Authorize(ctx context.Context, sub *user.User, obj string, act string) error {
+	logger := *zerolog.Ctx(ctx)
+
+	const (
+		ping   string = "/api/v1/ping"
+		movies string = "/api/v1/movies"
+	)
+
+	var authorized bool
+	switch obj == ping && act == http.MethodGet {
+	case true:
+		switch sub.Email {
+		case "gilcrest@gmail.com":
+			authorized = true
+		}
+	}
+
+	switch obj == movies && act == http.MethodPost {
+	case true:
+		switch sub.Email {
+		case "gilcrest@gmail.com":
+			authorized = true
+		}
+	}
+
+	if authorized {
+		logger.Info().Str("sub", sub.Email).Str("obj", obj).Str("act", act).Msgf("Authorization Granted")
+		return nil
+	}
+
+	logger.Info().Str("sub", sub.Email).Str("obj", obj).Str("act", act).Msgf("Authorization Denied")
 
 	// "In summary, a 401 Unauthorized response should be used for missing or
 	// bad authentication, and a 403 Forbidden response should be used afterwards,
@@ -23,14 +93,35 @@ func AuthorizeUser(ctx context.Context, u *user.User) error {
 	// requested operation on the given resource."
 	// If the user has gotten here, they have gotten through authentication
 	// but do have the right access, this they are Unauthorized
-	switch {
-	case exists && !authorized:
-		// User exists, but is not authorized
-		return errs.E(op, errs.Unauthorized, fmt.Sprintf("user email %s exists in auth map, but not allowed", u.Email))
-	case !exists:
-		// User does not exist in authorization list
-		return errs.E(op, errs.Unauthorized, fmt.Sprintf("user email %s does not exist in auth map", u.Email))
+	return errs.E(errs.Unauthorized, errors.New(fmt.Sprintf("user %s does not have %s permission for %s", sub.Email, act, obj)))
+}
+
+type contextKey string
+
+const contextKeyAccessToken = contextKey("access-token")
+
+// FromRequest gets the access token from the request
+func FromRequest(r *http.Request) (AccessToken, error) {
+	at := AccessToken{}
+	// retrieve the context from the http.Request
+	ctx := r.Context()
+
+	at, ok := ctx.Value(contextKeyAccessToken).(AccessToken)
+	if !ok {
+		return at, errs.E(errs.Unauthenticated, errors.New("Access Token not set properly to context"))
+	}
+	if at.Token == "" {
+		return at, errs.E(errs.Unauthenticated, errors.New("Access Token empty in context"))
+	}
+	return at, nil
+}
+
+// SetAccessToken2Context sets the Access Token to the given context
+func SetAccessToken2Context(ctx context.Context, token, tokenType string) context.Context {
+	at := AccessToken{
+		Token:     token,
+		TokenType: tokenType,
 	}
 
-	return nil
+	return context.WithValue(ctx, contextKeyAccessToken, at)
 }
