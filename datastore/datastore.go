ba3b1459245527d57fb88d732d77825db729d6bf
Dan Gillis <gilcrest@gmail.com>
2020-12-10 13:07:05 -0500

    use struct instead of map for datasource name

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -5,21 +5,27 @@ import (
 	"database/sql"
 	"fmt"
 	"os"
+	"strconv"
 
-	"github.com/gilcrest/errs"
+	"github.com/gilcrest/go-api-basic/domain/errs"
 
 	_ "github.com/lib/pq" // pq driver calls for blank identifier
+	"github.com/pkg/errors"
 )
 
 // Datastorer is an interface for working with the Database
 type Datastorer interface {
+	// DB returns a sql.DB
 	DB() *sql.DB
+	// BeginTx starts a sql.Tx using the input context
 	BeginTx(context.Context) (*sql.Tx, error)
+	// RollbackTx rolls back the input sql.Tx
 	RollbackTx(*sql.Tx, error) error
+	// CommitTx commits the Tx
 	CommitTx(*sql.Tx) error
 }
 
-// DSName defines the name for the Datastore
+// Name defines the name for the Datastore
 type Name int
 
 const (
@@ -49,9 +55,26 @@ func (n Name) String() string {
 	return "unknown_datastore_name"
 }
 
-func dbEnv(n Name) (map[string]string, error) {
-	const op errs.Op = "datastore/dbEnv"
+// PGDatasourceName is a Postgres datasource name
+type PGDatasourceName struct {
+	Host     string
+	Port     int
+	DBName   string
+	User     string
+	Password string
+}
 
+func (dsn PGDatasourceName) String() string {
+	// Craft string for database connection
+	switch dsn.Password {
+	case "":
+		return fmt.Sprintf("host=%s port=%d dbname=%s user=%s sslmode=disable", dsn.Host, dsn.Port, dsn.DBName, dsn.User)
+	default:
+		return fmt.Sprintf("host=%s port=%d dbname=%s user=%s password=%s sslmode=disable", dsn.Host, dsn.Port, dsn.DBName, dsn.User, dsn.Password)
+	}
+}
+
+func NewPGDatasourceName(n Name) (PGDatasourceName, error) {
 	// Constants for the local PostgreSQL Database connection
 	const (
 		localDBHost     string = "PG_APP_HOST"
@@ -89,89 +112,97 @@ func dbEnv(n Name) (map[string]string, error) {
 		port     string
 	)
 
+	var pgds PGDatasourceName
+
 	switch n {
 	case LocalDatastore:
 		host, ok = os.LookupEnv(localDBHost)
 		if !ok {
-			return nil, errs.E(op, fmt.Sprintf("No environment variable found for %s", localDBHost))
+			return pgds, errs.E(errors.New(fmt.Sprintf("No environment variable found for %s", localDBHost)))
 		}
 		port, ok = os.LookupEnv(localDBPort)
 		if !ok {
-			return nil, errs.E(op, fmt.Sprintf("No environment variable found for %s", localDBPort))
+			return pgds, errs.E(errors.New(fmt.Sprintf("No environment variable found for %s", localDBPort)))
 		}
 		dbName, ok = os.LookupEnv(localDBName)
 		if !ok {
-			return nil, errs.E(op, fmt.Sprintf("No environment variable found for %s", localDBName))
+			return pgds, errs.E(errors.New(fmt.Sprintf("No environment variable found for %s", localDBName)))
 		}
 		user, ok = os.LookupEnv(localDBUser)
 		if !ok {
-			return nil, errs.E(op, fmt.Sprintf("No environment variable found for %s", localDBUser))
+			return pgds, errs.E(errors.New(fmt.Sprintf("No environment variable found for %s", localDBUser)))
 		}
 		password, ok = os.LookupEnv(localDBPassword)
 		if !ok {
-			return nil, errs.E(op, fmt.Sprintf("No environment variable found for %s", localDBPassword))
+			return pgds, errs.E(errors.New(fmt.Sprintf("No environment variable found for %s", localDBPassword)))
 		}
 	case GCPCPDatastore:
 		host, ok = os.LookupEnv(gcpCPDBHost)
 		if !ok {
-			return nil, errs.E(op, fmt.Sprintf("No environment variable found for %s", gcpCPDBHost))
+			return pgds, errs.E(errors.New(fmt.Sprintf("No environment variable found for %s", gcpCPDBHost)))
 		}
 		port, ok = os.LookupEnv(gcpCPDBPort)
 		if !ok {
-			return nil, errs.E(op, fmt.Sprintf("No environment variable found for %s", gcpCPDBPort))
+			return pgds, errs.E(errors.New(fmt.Sprintf("No environment variable found for %s", gcpCPDBPort)))
 		}
 		dbName, ok = os.LookupEnv(gcpCPDBName)
 		if !ok {
-			return nil, errs.E(op, fmt.Sprintf("No environment variable found for %s", gcpCPDBName))
+			return pgds, errs.E(errors.New(fmt.Sprintf("No environment variable found for %s", gcpCPDBName)))
 		}
 		user, ok = os.LookupEnv(gcpCPDBUser)
 		if !ok {
-			return nil, errs.E(op, fmt.Sprintf("No environment variable found for %s", gcpCPDBUser))
+			return pgds, errs.E(errors.New(fmt.Sprintf("No environment variable found for %s", gcpCPDBUser)))
 		}
 		password, ok = os.LookupEnv(gcpCPDBPassword)
 		if !ok {
-			return nil, errs.E(op, fmt.Sprintf("No environment variable found for %s", gcpCPDBPassword))
+			return pgds, errs.E(errors.New(fmt.Sprintf("No environment variable found for %s", gcpCPDBPassword)))
 		}
 	case GCPDatastore:
 		host, ok = os.LookupEnv(gcpDBHost)
 		if !ok {
-			return nil, errs.E(op, fmt.Sprintf("No environment variable found for %s", gcpDBHost))
+			return pgds, errs.E(errors.New(fmt.Sprintf("No environment variable found for %s", gcpDBHost)))
 		}
 		port, ok = os.LookupEnv(gcpDBPort)
 		if !ok {
-			return nil, errs.E(op, fmt.Sprintf("No environment variable found for %s", gcpDBPort))
+			return pgds, errs.E(errors.New(fmt.Sprintf("No environment variable found for %s", gcpDBPort)))
 		}
 		dbName, ok = os.LookupEnv(gcpDBName)
 		if !ok {
-			return nil, errs.E(op, fmt.Sprintf("No environment variable found for %s", gcpDBName))
+			return pgds, errs.E(errors.New(fmt.Sprintf("No environment variable found for %s", gcpDBName)))
 		}
 		user, ok = os.LookupEnv(gcpDBUser)
 		if !ok {
-			return nil, errs.E(op, fmt.Sprintf("No environment variable found for %s", gcpDBUser))
+			return pgds, errs.E(errors.New(fmt.Sprintf("No environment variable found for %s", gcpDBUser)))
 		}
 		password, ok = os.LookupEnv(gcpDBPassword)
 		if !ok {
-			return nil, errs.E(op, fmt.Sprintf("No environment variable found for %s", gcpDBPassword))
+			return pgds, errs.E(errors.New(fmt.Sprintf("No environment variable found for %s", gcpDBPassword)))
 		}
 	default:
-		return nil, errs.E(op, "Unrecognized DSName")
+		return pgds, errs.E(errors.New("Unrecognized Name"))
 	}
 
-	dbEnvMap := map[string]string{
-		"dbname":   dbName,
-		"user":     user,
-		"password": password,
-		"host":     host,
-		"port":     port}
+	dbPort, err := strconv.Atoi(port)
+	if err != nil {
+		return pgds, errs.E(errors.New(fmt.Sprintf("Unable to convert db port %s to int", port)))
+	}
 
-	return dbEnvMap, nil
+	pgds = PGDatasourceName{
+		DBName:   dbName,
+		User:     user,
+		Password: password,
+		Host:     host,
+		Port:     dbPort,
+	}
+
+	return pgds, nil
 }
 
 func NewDatastore(db *sql.DB) *Datastore {
 	return &Datastore{db: db}
 }
 
-// Datastore is a concrete implementation for a database
+// Datastore is a concrete implementation for a sql database
 type Datastore struct {
 	db *sql.DB
 }
@@ -183,15 +214,13 @@ func (ds *Datastore) DB() *sql.DB {
 // BeginTx is a wrapper for sql.DB.BeginTx in order to expose from
 // the Datastore interface
 func (ds *Datastore) BeginTx(ctx context.Context) (*sql.Tx, error) {
-	const op errs.Op = "datastore/Datastore.BeginTx"
-
 	if ds.db == nil {
-		return nil, errs.E(op, errs.Database, "DB cannot be nil")
+		return nil, errs.E(errs.Database, errors.New("DB cannot be nil"))
 	}
 
 	tx, err := ds.db.BeginTx(ctx, nil)
 	if err != nil {
-		return nil, errs.E(op, errs.Database, err)
+		return nil, errs.E(errs.Database, err)
 	}
 
 	return tx, nil
@@ -200,28 +229,24 @@ func (ds *Datastore) BeginTx(ctx context.Context) (*sql.Tx, error) {
 // RollbackTx is a wrapper for sql.Tx.Rollback in order to expose from
 // the Datastore interface. Proper error handling is also considered.
 func (ds *Datastore) RollbackTx(tx *sql.Tx, err error) error {
-	const op errs.Op = "datastore/Datastore.RollbackTx"
-
 	if tx == nil {
-		return errs.E(op, errs.Database, "tx cannot be nil")
+		return errs.E(errs.Database, errors.New("tx cannot be nil"))
 	}
 
 	// Attempt to rollback the transaction
 	if rollbackErr := tx.Rollback(); rollbackErr != nil {
-		return errs.E(op, errs.Database, err)
+		return errs.E(errs.Database, rollbackErr)
 	}
 
 	// If rollback was successful, send back original error
-	return errs.E(op, errs.Database, err)
+	return errs.E(errs.Database, err)
 }
 
 // CommitTx is a wrapper for sql.Tx.Commit in order to expose from
 // the Datastore interface. Proper error handling is also considered.
 func (ds *Datastore) CommitTx(tx *sql.Tx) error {
-	const op errs.Op = "datastore/Datastore.CommitTx"
-
 	if err := tx.Commit(); err != nil {
-		return errs.E(op, errs.Database, err)
+		return errs.E(errs.Database, err)
 	}
 
 	return nil
