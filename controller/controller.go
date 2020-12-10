e842c7964c548349564e87cb2e50453f54d2b6ec
Dan Gillis <gilcrest@gmail.com>
2020-12-10 12:49:43 -0500

    error handling changes

--- controller/controller.go
+++ controller/controller.go
@@ -1,44 +1,54 @@
 package controller
 
 import (
-	"fmt"
+	"io"
 	"net/http"
 
-	"github.com/rs/xid"
+	"github.com/pkg/errors"
+
+	"github.com/gilcrest/go-api-basic/domain/errs"
+	"github.com/rs/zerolog/hlog"
 )
 
-// RequestID is the unique Request ID for each request
-type TraceID struct {
-	xid.ID
-}
-
-// StandardResponseFields is meant to be included in all response bodies
+// StandardResponse is meant to be included in all response bodies
 // and includes "standard" response fields
-type StandardResponseFields struct {
-	Path    string `json:"path,omitempty"`
-	TraceID string `json:"trace_id,omitempty"`
+type StandardResponse struct {
+	Path    string      `json:"path,omitempty"`
+	TraceID string      `json:"trace_id,omitempty"`
+	Data    interface{} `json:"data"`
 }
 
-// NewTraceID is an initializer for TraceID
-func NewTraceID(id xid.ID) TraceID {
-	return TraceID{ID: id}
-}
-
-// NewMockTraceID is an initializer for TraceID which returns a
-// static "mocked" TraceID
-func NewMockTraceID() TraceID {
-	x, err := xid.FromString("bpa182jipt3b2b78879g")
-	if err != nil {
-		fmt.Println(err)
-	}
-	return TraceID{ID: x}
-}
-
-// NewStandardResponseFields is an initializer for the StandardResponseFields struct
-func NewStandardResponseFields(id TraceID, r *http.Request) StandardResponseFields {
-	var sr StandardResponseFields
-	sr.TraceID = id.String()
+// NewStandardResponse is an initializer for the StandardResponse struct
+func NewStandardResponse(r *http.Request, d interface{}) (*StandardResponse, error) {
+	var sr StandardResponse
 	sr.Path = r.URL.EscapedPath()
+	// gets Trace ID from request
+	id, ok := hlog.IDFromRequest(r)
+	if !ok {
+		return nil, errs.E(errors.New("trace ID not properly set to request context"))
+	}
+	sr.TraceID = id.String()
 
-	return sr
+	sr.Data = d
+
+	return &sr, nil
+}
+
+// DecoderErr handles an error returned by json.NewDecoder(r.Body).Decode(&data)
+// this function will determine the appropriate error response
+func DecoderErr(err error) error {
+	switch {
+	// If the request body is empty (io.EOF)
+	// return an error
+	case err == io.EOF:
+		return errs.E(errs.InvalidRequest, errors.New("Request Body cannot be empty"))
+	// If the request body has malformed JSON (io.ErrUnexpectedEOF)
+	// return an error
+	case err == io.ErrUnexpectedEOF:
+		return errs.E(errs.InvalidRequest, errors.New("Malformed JSON"))
+	// return all other errors
+	case err != nil:
+		return errs.E(err)
+	}
+	return nil
 }
