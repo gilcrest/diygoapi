313aa86d40b3a5d1e13b19d73cfd33cd7518ff88
Dan Gillis <gilcrest@gmail.com>
2020-12-16 23:47:19 -0500

    remove comment

--- scripts/setlocalEnvVars.sh
+++ scripts/setlocalEnvVars.sh
@@ -4,12 +4,8 @@
 # you need to "source" the script, so run as either:
 # ". ./setlocalEnvVars.sh" or "source ./setlocalEnvVars.sh"
 
-# Local
 export PG_APP_DBNAME="go_api_basic"
 export PG_APP_USERNAME="postgres"
 export PG_APP_PASSWORD=""
 export PG_APP_HOST="localhost"
 export PG_APP_PORT="5432"
-
-# Sendgrid
-export SENDGRID_API_KEY='SG.allfake--FTf'
