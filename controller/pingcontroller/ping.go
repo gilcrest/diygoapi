65f660e0cc0823be5dc5d21bb5c6475d43d3a5c3
Dan Gillis <gilcrest@gmail.com>
2020-12-16 23:43:35 -0500

    actually call db ping

--- controller/pingcontroller/ping.go
+++ controller/pingcontroller/ping.go
@@ -4,15 +4,9 @@ import (
 	"net/http"
 
 	"github.com/gilcrest/go-api-basic/controller"
+	"github.com/rs/zerolog/hlog"
 
 	"github.com/gilcrest/go-api-basic/app"
-	"github.com/gilcrest/go-api-basic/domain/auth"
-	"github.com/gilcrest/go-api-basic/domain/errs"
-	"github.com/gilcrest/go-api-basic/gateway/authgateway"
-
-	"golang.org/x/oauth2"
-	googleoauth2 "google.golang.org/api/oauth2/v2"
-	"google.golang.org/api/option"
 )
 
 // NewPingController initializes SearchController
@@ -30,47 +24,39 @@ type PingResponseData struct {
 	DBUp bool `json:"db_up"`
 }
 
-func (ctl *PingController) newPingResponseData() *PingResponseData {
-	return &PingResponseData{DBUp: true}
+func newPingResponseData(dbup bool) *PingResponseData {
+	return &PingResponseData{DBUp: dbup}
 }
 
 // Ping is a simple check to make sure the API is up and running
 func (ctl *PingController) Ping(r *http.Request) (*controller.StandardResponse, error) {
+	// pull logger from request context
+	logger := hlog.FromRequest(r)
+	// pull the context from the http request
 	ctx := r.Context()
 
-	accessToken, err := auth.FromRequest(r)
+	// check if db connection is still alive
+	var dbok bool
+	err := ctl.App.Datastorer.DB().PingContext(ctx)
+	// if there is no error, db is up, set dbok to true,
+	// if db is down, log the error
 	if err != nil {
-		return nil, err
+		logger.Error().Err(err).Msg("PingContext returned an error")
+	} else {
+		dbok = true
 	}
 
-	oauthService, err := googleoauth2.NewService(ctx, option.WithTokenSource(oauth2.StaticTokenSource(accessToken.NewGoogleOauth2Token())))
-	if err != nil {
-		return nil, errs.E(err)
+	// if db is down, respond as such
+	if !dbok {
+		response, err := controller.NewStandardResponse(r, newPingResponseData(false))
+		if err != nil {
+			return nil, err
+		}
+		return response, nil
 	}
 
-	userInfo, err := oauthService.Userinfo.Get().Do()
-	if err != nil {
-		// "In summary, a 401 Unauthorized response should be used for missing or
-		// bad authentication, and a 403 Forbidden response should be used afterwards,
-		// when the user is authenticated but isn’t authorized to perform the
-		// requested operation on the given resource."
-		// In this case, we are getting a bad response from Google service, assume
-		// they are not able to authenticate properly
-		return nil, errs.E(errs.Unauthenticated, err)
-	}
-
-	u := authgateway.NewUser(userInfo)
-
-	var authorizer auth.Authorizer = auth.Auth{}
-	err = authorizer.Authorize(ctx, u, r.URL.Path, r.Method)
-	if err != nil {
-		return nil, err
-	}
-
-	// Populate the response
-	pr := ctl.newPingResponseData()
-
-	response, err := controller.NewStandardResponse(r, pr)
+	// if db is up, respond as such
+	response, err := controller.NewStandardResponse(r, newPingResponseData(true))
 	if err != nil {
 		return nil, err
 	}
