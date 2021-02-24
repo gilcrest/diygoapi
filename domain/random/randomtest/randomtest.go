2238546a4ed593da118ca74445570fa7e7f17654
Dan Gillis <gilcrest@gmail.com>
2021-02-24 15:19:06 -0500

    Initial commit

---
+++ domain/random/randomtest/randomtest.go
@@ -0,0 +1,20 @@
+package randomtest
+
+import "testing"
+
+// NewMockStringGenerator is an initializer for MockStringGenerator
+func NewMockStringGenerator(t *testing.T) MockStringGenerator {
+	return MockStringGenerator{t: t}
+}
+
+// MockStringGenerator creates a static string for testing
+type MockStringGenerator struct {
+	t *testing.T
+}
+
+// CryptoString creates a static string for testing
+func (g MockStringGenerator) CryptoString(n int) (string, error) {
+	g.t.Helper()
+
+	return "superRandomString", nil
+}
