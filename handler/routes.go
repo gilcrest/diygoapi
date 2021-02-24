08871018035fda4dd6224085086077509cda47d8
Dan Gillis <gilcrest@gmail.com>
2021-02-24 00:05:25 -0500

    use constants

--- handler/routes.go
+++ handler/routes.go
@@ -1,9 +1,16 @@
 package handler
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/justinas/alice"
 	"github.com/rs/zerolog"
+)
+
+const (
+	pathPrefix       string = "/api"
+	moviesV1PathRoot string = "/v1/movies"
 )
 
 // NewMuxRouter sets up the mux.Router and registers routes to URL paths
@@ -23,52 +30,52 @@ func NewMuxRouter(logger zerolog.Logger, handlers Handlers) *mux.Router {
 	// sure that every request has "/api" as part of it's path prefix
 	// without having to put it into every handle path in my various
 	// routing functions
-	rtr = rtr.PathPrefix("/api").Subrouter()
+	rtr = rtr.PathPrefix(pathPrefix).Subrouter()
 
 	// Match only POST requests at /api/v1/movies
 	// with Content-Type header = application/json
-	rtr.Handle("/v1/movies",
+	rtr.Handle(moviesV1PathRoot,
 		c.Append(AccessTokenHandler).
 			Append(JSONContentTypeHandler).
 			Then(handlers.CreateMovieHandler)).
-		Methods("POST").
+		Methods(http.MethodPost).
 		Headers("Content-Type", "application/json")
 
 	// Match only PUT requests having an ID at /api/v1/movies/{id}
 	// with the Content-Type header = application/json
-	rtr.Handle("/v1/movies/{id}",
+	rtr.Handle(moviesV1PathRoot+"/{id}",
 		c.Append(AccessTokenHandler).
 			Append(JSONContentTypeHandler).
 			Then(handlers.UpdateMovieHandler)).
-		Methods("PUT").
+		Methods(http.MethodPut).
 		Headers("Content-Type", "application/json")
 
 	// Match only DELETE requests having an ID at /api/v1/movies/{id}
-	rtr.Handle("/v1/movies/{id}",
+	rtr.Handle(moviesV1PathRoot+"/{id}",
 		c.Append(AccessTokenHandler).
 			Append(JSONContentTypeHandler).
 			Then(handlers.DeleteMovieHandler)).
-		Methods("DELETE")
+		Methods(http.MethodDelete)
 
 	// Match only GET requests having an ID at /api/v1/movies/{id}
-	rtr.Handle("/v1/movies/{id}",
+	rtr.Handle(moviesV1PathRoot+"/{id}",
 		c.Append(AccessTokenHandler).
 			Append(JSONContentTypeHandler).
 			Then(handlers.FindMovieByIDHandler)).
-		Methods("GET")
+		Methods(http.MethodGet)
 
 	// Match only GET requests /api/v1/movies
-	rtr.Handle("/v1/movies",
+	rtr.Handle(moviesV1PathRoot,
 		c.Append(AccessTokenHandler).
 			Append(JSONContentTypeHandler).
 			Then(handlers.FindAllMoviesHandler)).
-		Methods("GET")
+		Methods(http.MethodGet)
 
 	// Match only GET requests at /api/v1/ping
 	rtr.Handle("/v1/ping",
 		c.Append(JSONContentTypeHandler).
 			Then(handlers.PingHandler)).
-		Methods("GET")
+		Methods(http.MethodGet)
 
 	return rtr
 }
