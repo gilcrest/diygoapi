ed74438692010351017244c1be74902adf568cdd
Dan Gillis <gilcrest@gmail.com>
2021-02-24 15:41:15 -0500

    use environment variable for oauth2 access token testing

--- scripts/setlocalEnvVars.sh
+++ scripts/setlocalEnvVars.sh
@@ -11,5 +11,6 @@ export PG_APP_PASSWORD=""
 export PG_APP_HOST="localhost"
 export PG_APP_PORT="5432"
 
-# Google Oauth2 token
-export GOOGLE_ACCESS_TOKEN="ya29.A0AfH6SMBXbKWdkWYVzzzTw7UC4pSFdbBk9s7Mnb1E-rqOkEZx6vScm8PgUQXVN57BcpqaO7aDR4aCxSo8907gGI9K-PdWxy_lDmvhw-fh4rbhROE6S6o_XefZJD3o0_VvnpEM7rERPGrYvSkjvU0_ygb3eBWx"
+# Google Oauth2 token - use the Google oauth2 playground https://developers.google.com/oauthplayground/
+# and replace below
+export GOOGLE_ACCESS_TOKEN="REPLACEME"
