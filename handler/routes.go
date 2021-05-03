38c2ceb12f1851b15dd86e9a0782e51305e7b7b8
Dan Gillis <gilcrest@gmail.com>
2021-05-03 14:19:15 -0400

    Fix chain bug

--- handler/routes.go
+++ handler/routes.go
@@ -23,7 +23,7 @@ const (
 
 // NewMuxRouter initializes a gorilla/mux router and registers routes
 // and corresponding middleware/handlers to it
-func NewMuxRouter(logger zerolog.Logger, mw Middleware, handlers Handlers) *mux.Router {
+func NewMuxRouter(lgr zerolog.Logger, mw Middleware, handlers Handlers) *mux.Router {
 
 	// initializer gorilla/mux router
 	rtr := mux.NewRouter()
@@ -31,8 +31,10 @@ func NewMuxRouter(logger zerolog.Logger, mw Middleware, handlers Handlers) *mux.
 	// Start a new alice handler chain
 	c := alice.New()
 
-	// add loggerHandlerChain handler chain and zerolog logger to Context
-	c = loggerHandlerChain(logger, c)
+	// add logger middleware chain; add zerolog to Context
+	chainWithLogger := c.Extend(loggerChain(lgr))
+
+	//chainWithLoggerAuth := chainWithLogger.Extend(authChain(mw))
 
 	// send Router through PathPrefix method to validate any standard
 	// subroutes you may want for your APIs. e.g. I always want to be
@@ -44,7 +46,7 @@ func NewMuxRouter(logger zerolog.Logger, mw Middleware, handlers Handlers) *mux.
 	// Match only POST requests at /api/v1/movies
 	// with Content-Type header = application/json
 	rtr.Handle(moviesV1PathRoot,
-		c.Extend(authChain(mw, c)).
+		chainWithLogger.Extend(authChain(mw)).
 			Append(mw.JSONContentTypeResponseMiddleware()).
 			Then(handlers.CreateMovieHandler)).
 		Methods(http.MethodPost).
@@ -53,7 +55,7 @@ func NewMuxRouter(logger zerolog.Logger, mw Middleware, handlers Handlers) *mux.
 	// Match only PUT requests having an ID at /api/v1/movies/{id}
 	// with the Content-Type header = application/json
 	rtr.Handle(moviesV1PathRoot+extlIDPathDir,
-		c.Extend(authChain(mw, c)).
+		chainWithLogger.Extend(authChain(mw)).
 			Append(mw.JSONContentTypeResponseMiddleware()).
 			Then(handlers.UpdateMovieHandler)).
 		Methods(http.MethodPut).
@@ -61,35 +63,35 @@ func NewMuxRouter(logger zerolog.Logger, mw Middleware, handlers Handlers) *mux.
 
 	// Match only DELETE requests having an ID at /api/v1/movies/{id}
 	rtr.Handle(moviesV1PathRoot+extlIDPathDir,
-		c.Extend(authChain(mw, c)).
+		chainWithLogger.Extend(authChain(mw)).
 			Append(mw.JSONContentTypeResponseMiddleware()).
 			Then(handlers.DeleteMovieHandler)).
 		Methods(http.MethodDelete)
 
 	// Match only GET requests having an ID at /api/v1/movies/{id}
 	rtr.Handle(moviesV1PathRoot+extlIDPathDir,
-		c.Extend(authChain(mw, c)).
+		chainWithLogger.Extend(authChain(mw)).
 			Append(mw.JSONContentTypeResponseMiddleware()).
 			Then(handlers.FindMovieByIDHandler)).
 		Methods(http.MethodGet)
 
 	// Match only GET requests /api/v1/movies
 	rtr.Handle(moviesV1PathRoot,
-		c.Extend(authChain(mw, c)).
+		chainWithLogger.Extend(authChain(mw)).
 			Append(mw.JSONContentTypeResponseMiddleware()).
 			Then(handlers.FindAllMoviesHandler)).
 		Methods(http.MethodGet)
 
 	// Match only GET requests /api/v1/logger
 	rtr.Handle(loggerV1PathRoot,
-		c.Extend(authChain(mw, c)).
+		chainWithLogger.Extend(authChain(mw)).
 			Append(mw.JSONContentTypeResponseMiddleware()).
 			Then(handlers.ReadLoggerHandler)).
 		Methods(http.MethodGet)
 
 	// Match only PUT requests /api/v1/logger
 	rtr.Handle(loggerV1PathRoot,
-		c.Extend(authChain(mw, c)).
+		chainWithLogger.Extend(authChain(mw)).
 			Append(mw.JSONContentTypeResponseMiddleware()).
 			Then(handlers.UpdateLoggerHandler)).
 		Methods(http.MethodPut).
@@ -97,48 +99,45 @@ func NewMuxRouter(logger zerolog.Logger, mw Middleware, handlers Handlers) *mux.
 
 	// Match only GET requests at /api/v1/ping
 	rtr.Handle(pingV1PathRoot,
-		c.Append(mw.JSONContentTypeResponseMiddleware()).
+		chainWithLogger.Append(mw.JSONContentTypeResponseMiddleware()).
 			Then(handlers.PingHandler)).
 		Methods(http.MethodGet)
 
 	return rtr
 }
 
-// loggerHandlerChain returns a handler chain (via alice.Chain)
-// initialized with all the standard handlers for logging. The logger
+// loggerChain returns a middleware chain (via alice.Chain)
+// initialized with all the standard middleware handlers for logging. The logger
 // will be added to the request context for subsequent use with pre-populated
 // fields, including the request method, url, status, size, duration, remote IP,
 // user agent, referer. A unique Request ID is also added to the logger, context
 // and response headers.
-func loggerHandlerChain(logger zerolog.Logger, c alice.Chain) alice.Chain {
+func loggerChain(logger zerolog.Logger) alice.Chain {
 
-	// Install the logger handler with default output on the console
-	c = c.Append(hlog.NewHandler(logger))
+	ac := alice.New(hlog.NewHandler(logger),
+		hlog.AccessHandler(func(r *http.Request, status, size int, duration time.Duration) {
+			hlog.FromRequest(r).Info().
+				Str("method", r.Method).
+				Stringer("url", r.URL).
+				Int("status", status).
+				Int("size", size).
+				Dur("duration", duration).
+				Msg("request logged")
+		}),
+		hlog.RemoteAddrHandler("remote_ip"),
+		hlog.UserAgentHandler("user_agent"),
+		hlog.RefererHandler("referer"),
+		hlog.RequestIDHandler("request_id", "Request-Id"),
+	)
 
-	// Install extra handler to set request's context fields.
-	// Thanks to that handler, all our logs will come with some pre-populated fields.
-	c = c.Append(hlog.AccessHandler(func(r *http.Request, status, size int, duration time.Duration) {
-		hlog.FromRequest(r).Info().
-			Str("method", r.Method).
-			Stringer("url", r.URL).
-			Int("status", status).
-			Int("size", size).
-			Dur("duration", duration).
-			Msg("")
-	})).
-		Append(hlog.RemoteAddrHandler("remote_ip")).
-		Append(hlog.UserAgentHandler("user_agent")).
-		Append(hlog.RefererHandler("referer")).
-		Append(hlog.RequestIDHandler("request_id", "Request-Id"))
-
-	return c
+	return ac
 }
 
 // authChain chains the handlers together for User authorization
-func authChain(mw Middleware, c alice.Chain) alice.Chain {
-	c = c.Append(mw.AccessTokenMiddleware()).
-		Append(mw.ConvertAccessTokenMiddleware()).
-		Append(mw.AuthorizeUserMiddleware())
+func authChain(mw Middleware) alice.Chain {
+	ac := alice.New(mw.AccessTokenMiddleware(),
+		mw.ConvertAccessTokenMiddleware(),
+		mw.AuthorizeUserMiddleware())
 
-	return c
+	return ac
 }
