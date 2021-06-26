676a53e313339c529cc8a8c38a7e16437ae7c092
Dan Gillis <gilcrest@gmail.com>
2021-06-26 17:01:42 -0400

    json tags unnecessary. close #19

--- domain/user/user.go
+++ domain/user/user.go
@@ -11,29 +11,26 @@ import (
 // User holds details of a User from Google
 type User struct {
 	// Email: The user's email address.
-	Email string `json:"email,omitempty"`
+	Email string
 
 	// LastName: The user's last name.
-	LastName string `json:"last_name,omitempty"`
+	LastName string
 
 	// FirstName: The user's first name.
-	FirstName string `json:"first_name,omitempty"`
+	FirstName string
 
 	// FullName: The user's full name.
-	FullName string `json:"full_name,omitempty"`
-
-	// Gender: The user's gender.
-	//Gender string `json:"gender,omitempty"`
+	FullName string
 
 	// HostedDomain: The hosted domain e.g. example.com if the user
 	// is Google apps user.
-	HostedDomain string `json:"hosted_domain,omitempty"`
+	HostedDomain string
 
 	// PictureURL: URL of the user's picture image.
-	PictureURL string `json:"picture_url,omitempty"`
+	PictureURL string
 
 	// ProfileLink: URL of the profile page.
-	ProfileLink string `json:"profile_link,omitempty"`
+	ProfileLink string
 }
 
 // IsValid determines whether or not the User has proper
