e3525b55aac66d17895e609649e26aa4dab2e011
Dan Gillis <gilcrest@gmail.com>
2021-12-30 20:15:56 -0800

    Add app, org, seed, temp comment movie handlers.
    Refactoring. Will refactor and add back.

--- app/handlers.go
+++ app/handlers.go
@@ -5,27 +5,229 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-
-	"github.com/gilcrest/go-api-basic/domain/user"
-
 	"github.com/rs/zerolog/hlog"
 
+	"github.com/gilcrest/go-api-basic/domain/audit"
 	"github.com/gilcrest/go-api-basic/domain/errs"
 	"github.com/gilcrest/go-api-basic/service"
 )
 
-// CreateMovie is a HandlerFunc used to create a Movie
-func (s *Server) handleMovieCreate(w http.ResponseWriter, r *http.Request) {
+//// CreateMovie is a HandlerFunc used to create a Movie
+//func (s *Server) handleMovieCreate(w http.ResponseWriter, r *http.Request) {
+//	logger := *hlog.FromRequest(r)
+//
+//	u, err := user.FromRequest(r)
+//	if err != nil {
+//		errs.HTTPErrorResponse(w, logger, err)
+//		return
+//	}
+//
+//	// Declare request body (rb) as an instance of service.MovieRequest
+//	rb := new(service.CreateMovieRequest)
+//
+//	// Decode JSON HTTP request body into a Decoder type
+//	// and unmarshal that into the MovieRequest struct in the
+//	// AddMovieHandler
+//	err = json.NewDecoder(r.Body).Decode(&rb)
+//	defer r.Body.Close()
+//	// Call decoderErr to determine if body is nil, json is malformed
+//	// or any other error
+//	err = decoderErr(err)
+//	if err != nil {
+//		errs.HTTPErrorResponse(w, logger, err)
+//		return
+//	}
+//
+//	response, err := s.CreateMovieService.Create(r.Context(), rb, u)
+//	if err != nil {
+//		errs.HTTPErrorResponse(w, logger, err)
+//		return
+//	}
+//
+//	// Encode response struct to JSON for the response body
+//	err = json.NewEncoder(w).Encode(response)
+//	if err != nil {
+//		errs.HTTPErrorResponse(w, logger, errs.E(errs.Internal, err))
+//		return
+//	}
+//}
+//
+//// handleMovieUpdate handles PUT requests for the /movies/{id} endpoint
+//// and updates the given movie
+//func (s *Server) handleMovieUpdate(w http.ResponseWriter, r *http.Request) {
+//
+//	logger := *hlog.FromRequest(r)
+//
+//	u, err := user.FromRequest(r)
+//	if err != nil {
+//		errs.HTTPErrorResponse(w, logger, err)
+//		return
+//	}
+//
+//	// gorilla mux Vars function returns the route variables for the
+//	// current request, if any. id is the external id given for the
+//	// movie
+//	vars := mux.Vars(r)
+//	extlid := vars["extlID"]
+//
+//	// Declare request body (rb) as an instance of service.MovieRequest
+//	rb := new(service.UpdateMovieRequest)
+//
+//	// Decode JSON HTTP request body into a Decoder type
+//	// and unmarshal that into requestData
+//	err = json.NewDecoder(r.Body).Decode(&rb)
+//	defer r.Body.Close()
+//	// Call DecoderErr to determine if body is nil, json is malformed
+//	// or any other error
+//	err = decoderErr(err)
+//	if err != nil {
+//		errs.HTTPErrorResponse(w, logger, err)
+//		return
+//	}
+//
+//	// External ID is from path variable, need to set separate
+//	// from decoding response body
+//	rb.ExternalID = extlid
+//
+//	response, err := s.UpdateMovieService.Update(r.Context(), rb, u)
+//	if err != nil {
+//		errs.HTTPErrorResponse(w, logger, err)
+//		return
+//	}
+//
+//	// Encode response struct to JSON for the response body
+//	err = json.NewEncoder(w).Encode(response)
+//	if err != nil {
+//		errs.HTTPErrorResponse(w, logger, errs.E(errs.Internal, err))
+//		return
+//	}
+//}
+//
+//// handleMovieDelete handles DELETE requests for the /movies/{id} endpoint
+//// and updates the given movie
+//func (s *Server) handleMovieDelete(w http.ResponseWriter, r *http.Request) {
+//
+//	logger := *hlog.FromRequest(r)
+//
+//	// gorilla mux Vars function returns the route variables for the
+//	// current request, if any. id is the external id given for the
+//	// movie
+//	vars := mux.Vars(r)
+//	extlID := vars["extlID"]
+//
+//	response, err := s.DeleteMovieService.Delete(r.Context(), extlID)
+//	if err != nil {
+//		errs.HTTPErrorResponse(w, logger, err)
+//		return
+//	}
+//
+//	// Encode response struct to JSON for the response body
+//	err = json.NewEncoder(w).Encode(response)
+//	if err != nil {
+//		errs.HTTPErrorResponse(w, logger, errs.E(errs.Internal, err))
+//		return
+//	}
+//}
+//
+//// handleFindMovieByID handles GET requests for the /movies/{id} endpoint
+//// and finds a movie by its ID
+//func (s *Server) handleFindMovieByID(w http.ResponseWriter, r *http.Request) {
+//
+//	logger := *hlog.FromRequest(r)
+//
+//	// gorilla mux Vars function returns the route variables for the
+//	// current request, if any. id is the external id given for the
+//	// movie
+//	vars := mux.Vars(r)
+//	extlID := vars["extlID"]
+//
+//	response, err := s.FindMovieService.FindMovieByID(r.Context(), extlID)
+//	if err != nil {
+//		errs.HTTPErrorResponse(w, logger, err)
+//		return
+//	}
+//
+//	// Encode response struct to JSON for the response body
+//	err = json.NewEncoder(w).Encode(response)
+//	if err != nil {
+//		errs.HTTPErrorResponse(w, logger, errs.E(errs.Internal, err))
+//		return
+//	}
+//}
+//
+//// handleFindAllMovies handles GET requests for the /movies endpoint and finds
+//// all movies
+//func (s *Server) handleFindAllMovies(w http.ResponseWriter, r *http.Request) {
+//
+//	logger := *hlog.FromRequest(r)
+//
+//	response, err := s.FindMovieService.FindAllMovies(r.Context())
+//	if err != nil {
+//		errs.HTTPErrorResponse(w, logger, err)
+//		return
+//	}
+//
+//	// Encode response struct to JSON for the response body
+//	err = json.NewEncoder(w).Encode(response)
+//	if err != nil {
+//		errs.HTTPErrorResponse(w, logger, errs.E(errs.Internal, err))
+//		return
+//	}
+//}
+
+// handleOrgFindAll is a HandlerFunc used to find a list of Orgs
+func (s *Server) handleOrgFindAll(w http.ResponseWriter, r *http.Request) {
 	logger := *hlog.FromRequest(r)
 
-	u, err := user.FromRequest(r)
+	response, err := s.FindOrgService.FindAll(r.Context())
+	if err != nil {
+		errs.HTTPErrorResponse(w, logger, err)
+		return
+	}
+
+	// Encode response struct to JSON for the response body
+	err = json.NewEncoder(w).Encode(response)
+	if err != nil {
+		errs.HTTPErrorResponse(w, logger, errs.E(errs.Internal, err))
+		return
+	}
+}
+
+// handleOrgFindByExtlID is a HandlerFunc used to find a specific Org by External ID
+func (s *Server) handleOrgFindByExtlID(w http.ResponseWriter, r *http.Request) {
+	logger := *hlog.FromRequest(r)
+
+	// gorilla mux Vars function returns the route variables for the
+	// current request, if any. ID is the external id given for the resource
+	vars := mux.Vars(r)
+	extlID := vars["extlID"]
+
+	response, err := s.FindOrgService.FindByExternalID(r.Context(), extlID)
+	if err != nil {
+		errs.HTTPErrorResponse(w, logger, err)
+		return
+	}
+
+	// Encode response struct to JSON for the response body
+	err = json.NewEncoder(w).Encode(response)
+	if err != nil {
+		errs.HTTPErrorResponse(w, logger, errs.E(errs.Internal, err))
+		return
+	}
+}
+
+// handleOrgCreate is a HandlerFunc used to create an Org
+func (s *Server) handleOrgCreate(w http.ResponseWriter, r *http.Request) {
+	logger := *hlog.FromRequest(r)
+
+	adt, err := audit.FromRequest(r)
 	if err != nil {
 		errs.HTTPErrorResponse(w, logger, err)
 		return
 	}
 
 	// Declare request body (rb) as an instance of service.MovieRequest
-	rb := new(service.CreateMovieRequest)
+	rb := new(service.CreateOrgRequest)
 
 	// Decode JSON HTTP request body into a Decoder type
 	// and unmarshal that into the MovieRequest struct in the
@@ -40,7 +242,7 @@ func (s *Server) handleMovieCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	response, err := s.CreateMovieService.Create(r.Context(), rb, u)
+	response, err := s.CreateOrgService.Create(r.Context(), rb, adt)
 	if err != nil {
 		errs.HTTPErrorResponse(w, logger, err)
 		return
@@ -54,32 +256,25 @@ func (s *Server) handleMovieCreate(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// handleMovieUpdate handles PUT requests for the /movies/{id} endpoint
-// and updates the given movie
-func (s *Server) handleMovieUpdate(w http.ResponseWriter, r *http.Request) {
-
+// handleOrgUpdate is a HandlerFunc used to update an Org
+func (s *Server) handleOrgUpdate(w http.ResponseWriter, r *http.Request) {
 	logger := *hlog.FromRequest(r)
 
-	u, err := user.FromRequest(r)
+	adt, err := audit.FromRequest(r)
 	if err != nil {
 		errs.HTTPErrorResponse(w, logger, err)
 		return
 	}
 
-	// gorilla mux Vars function returns the route variables for the
-	// current request, if any. id is the external id given for the
-	// movie
-	vars := mux.Vars(r)
-	extlid := vars["extlID"]
-
 	// Declare request body (rb) as an instance of service.MovieRequest
-	rb := new(service.UpdateMovieRequest)
+	rb := new(service.UpdateOrgRequest)
 
 	// Decode JSON HTTP request body into a Decoder type
-	// and unmarshal that into requestData
+	// and unmarshal that into the MovieRequest struct in the
+	// AddMovieHandler
 	err = json.NewDecoder(r.Body).Decode(&rb)
 	defer r.Body.Close()
-	// Call DecoderErr to determine if body is nil, json is malformed
+	// Call decoderErr to determine if body is nil, json is malformed
 	// or any other error
 	err = decoderErr(err)
 	if err != nil {
@@ -87,37 +282,12 @@ func (s *Server) handleMovieUpdate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// External ID is from path variable, need to set separate
-	// from decoding response body
-	rb.ExternalID = extlid
-
-	response, err := s.UpdateMovieService.Update(r.Context(), rb, u)
-	if err != nil {
-		errs.HTTPErrorResponse(w, logger, err)
-		return
-	}
-
-	// Encode response struct to JSON for the response body
-	err = json.NewEncoder(w).Encode(response)
-	if err != nil {
-		errs.HTTPErrorResponse(w, logger, errs.E(errs.Internal, err))
-		return
-	}
-}
-
-// handleMovieDelete handles DELETE requests for the /movies/{id} endpoint
-// and updates the given movie
-func (s *Server) handleMovieDelete(w http.ResponseWriter, r *http.Request) {
-
-	logger := *hlog.FromRequest(r)
-
 	// gorilla mux Vars function returns the route variables for the
-	// current request, if any. id is the external id given for the
-	// movie
+	// current request, if any. ID is the external id given for the resource
 	vars := mux.Vars(r)
-	extlID := vars["extlID"]
+	rb.ExternalID = vars["extlID"]
 
-	response, err := s.DeleteMovieService.Delete(r.Context(), extlID)
+	response, err := s.UpdateOrgService.Update(r.Context(), rb, adt)
 	if err != nil {
 		errs.HTTPErrorResponse(w, logger, err)
 		return
@@ -131,39 +301,33 @@ func (s *Server) handleMovieDelete(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// handleFindMovieByID handles GET requests for the /movies/{id} endpoint
-// and finds a movie by its ID
-func (s *Server) handleFindMovieByID(w http.ResponseWriter, r *http.Request) {
-
+// handleAppCreate is a HandlerFunc used to create an App
+func (s *Server) handleAppCreate(w http.ResponseWriter, r *http.Request) {
 	logger := *hlog.FromRequest(r)
 
-	// gorilla mux Vars function returns the route variables for the
-	// current request, if any. id is the external id given for the
-	// movie
-	vars := mux.Vars(r)
-	extlID := vars["extlID"]
-
-	response, err := s.FindMovieService.FindMovieByID(r.Context(), extlID)
+	adt, err := audit.FromRequest(r)
 	if err != nil {
 		errs.HTTPErrorResponse(w, logger, err)
 		return
 	}
 
-	// Encode response struct to JSON for the response body
-	err = json.NewEncoder(w).Encode(response)
+	// Declare request body (rb)
+	rb := new(service.CreateAppRequest)
+
+	// Decode JSON HTTP request body into a Decoder type
+	// and unmarshal that into the MovieRequest struct in the
+	// AddMovieHandler
+	err = json.NewDecoder(r.Body).Decode(&rb)
+	defer r.Body.Close()
+	// Call decoderErr to determine if body is nil, json is malformed
+	// or any other error
+	err = decoderErr(err)
 	if err != nil {
-		errs.HTTPErrorResponse(w, logger, errs.E(errs.Internal, err))
+		errs.HTTPErrorResponse(w, logger, err)
 		return
 	}
-}
 
-// handleFindAllMovies handles GET requests for the /movies endpoint and finds
-// all movies
-func (s *Server) handleFindAllMovies(w http.ResponseWriter, r *http.Request) {
-
-	logger := *hlog.FromRequest(r)
-
-	response, err := s.FindMovieService.FindAllMovies(r.Context())
+	response, err := s.CreateAppService.Create(r.Context(), rb, adt)
 	if err != nil {
 		errs.HTTPErrorResponse(w, logger, err)
 		return
@@ -239,6 +403,40 @@ func (s *Server) handlePing(w http.ResponseWriter, r *http.Request) {
 	err := json.NewEncoder(w).Encode(response)
 	if err != nil {
 		errs.HTTPErrorResponse(w, logger, errs.E(errs.Internal, err))
+		return
+	}
+}
+
+// handleSeed handles POST requests for the /seed endpoint
+// and updates the logger globals
+func (s *Server) handleSeed(w http.ResponseWriter, r *http.Request) {
+	lgr := *hlog.FromRequest(r)
+
+	// Declare rb as an instance of service.LoggerRequest
+	rb := new(service.SeedRequest)
+
+	// Decode JSON HTTP request body into a json.Decoder type
+	// and unmarshal that into rb
+	err := json.NewDecoder(r.Body).Decode(&rb)
+	defer r.Body.Close()
+	// Call DecoderErr to determine if body is nil, json is malformed
+	// or any other error
+	err = decoderErr(err)
+	if err != nil {
+		errs.HTTPErrorResponse(w, lgr, err)
+		return
+	}
+
+	response, err := s.SeedService.Seed(r.Context(), rb)
+	if err != nil {
+		errs.HTTPErrorResponse(w, lgr, err)
+		return
+	}
+
+	// Encode response struct to JSON for the response body
+	err = json.NewEncoder(w).Encode(response)
+	if err != nil {
+		errs.HTTPErrorResponse(w, lgr, errs.E(errs.Internal, err))
 		return
 	}
 }
