d9fcc9a8431501fc907301097de34b953608b47d
Dan Gillis <gilcrest@gmail.com>
2022-01-21 17:06:58 -0500

    update tests

--- domain/auth/auth_test.go
+++ domain/auth/auth_test.go
@@ -1,177 +1,106 @@
-package auth
+package auth_test
 
 import (
-	"context"
 	"net/http"
+	"net/http/httptest"
 	"os"
-	"reflect"
 	"testing"
+	"time"
 
+	"github.com/casbin/casbin/v2"
+	qt "github.com/frankban/quicktest"
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
 	"github.com/rs/zerolog"
-	"golang.org/x/oauth2"
 
+	"github.com/gilcrest/go-api-basic/domain/app"
+	"github.com/gilcrest/go-api-basic/domain/audit"
+	"github.com/gilcrest/go-api-basic/domain/auth"
 	"github.com/gilcrest/go-api-basic/domain/logger"
+	"github.com/gilcrest/go-api-basic/domain/org"
+	"github.com/gilcrest/go-api-basic/domain/person"
 	"github.com/gilcrest/go-api-basic/domain/user"
-	"github.com/gilcrest/go-api-basic/domain/user/usertest"
 )
 
-func TestAccessToken_NewGoogleOauth2Token(t *testing.T) {
-	type fields struct {
-		Token     string
-		TokenType string
-	}
-
-	gtoken := &oauth2.Token{
-		AccessToken: "abcdef123",
-		TokenType:   BearerTokenType,
-	}
-
-	tests := []struct {
-		name   string
-		fields fields
-		want   *oauth2.Token
-	}{
-		{"typical", fields{Token: "abcdef123", TokenType: BearerTokenType}, gtoken},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			at := AccessToken{
-				Token:     tt.fields.Token,
-				TokenType: tt.fields.TokenType,
-			}
-			if got := at.NewGoogleOauth2Token(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewGoogleOauth2Token() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+func TestNewProvider(t *testing.T) {
+	t.Run("google", func(t *testing.T) {
+		c := qt.New(t)
+		p := auth.NewProvider("GoOgLe")
+		c.Assert(p, qt.Equals, auth.Google)
+	})
+	t.Run("apple", func(t *testing.T) {
+		c := qt.New(t)
+		p := auth.NewProvider("ApPlE")
+		c.Assert(p, qt.Equals, auth.Apple)
+	})
+	t.Run("invalid", func(t *testing.T) {
+		c := qt.New(t)
+		p := auth.NewProvider("anything else!")
+		c.Assert(p, qt.Equals, auth.Invalid)
+	})
 }
 
-func TestAuthorizer_Authorize(t *testing.T) {
-	type args struct {
-		lgr zerolog.Logger
-		sub user.User
-		obj string
-		act string
-	}
-
-	lgr := logger.NewLogger(os.Stdout, zerolog.DebugLevel, true)
-	u := usertest.NewUser(t)
-	invalidUser := user.User{Email: "badactor@gmail.com"}
-	obj := "/api/v1/movies"
-	act := http.MethodGet
-
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		{"typical", args{lgr, u, obj, act}, false},
-		{"invalid user", args{lgr, invalidUser, obj, act}, true},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			a := Authorizer{}
-			if err := a.Authorize(tt.args.lgr, tt.args.sub, tt.args.obj, tt.args.act); (err != nil) != tt.wantErr {
-				t.Errorf("Authorize() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
+func TestProvider_String(t *testing.T) {
+	t.Run("google", func(t *testing.T) {
+		c := qt.New(t)
+		p := auth.NewProvider("GoOgLe")
+		provider := p.String()
+		c.Assert(provider, qt.Equals, "google")
+	})
+	t.Run("apple", func(t *testing.T) {
+		c := qt.New(t)
+		p := auth.NewProvider("APPLe")
+		provider := p.String()
+		c.Assert(provider, qt.Equals, "apple")
+	})
+	t.Run("invalid", func(t *testing.T) {
+		c := qt.New(t)
+		p := auth.NewProvider("anything else")
+		provider := p.String()
+		c.Assert(provider, qt.Equals, "invalid_provider")
+	})
 }
 
-func TestCtxWithAccessToken(t *testing.T) {
-	type args struct {
-		ctx   context.Context
-		token AccessToken
-	}
-	ctx := context.Background()
-	token := "abcdef123"
+func TestCasbinAuthorizer_Authorize(t *testing.T) {
+	t.Run("valid user", func(t *testing.T) {
+		c := qt.New(t)
 
-	at := AccessToken{
-		Token:     token,
-		TokenType: BearerTokenType,
-	}
+		lgr := logger.NewLogger(os.Stdout, zerolog.DebugLevel, true)
+		req := httptest.NewRequest(http.MethodGet, "/api/v1/ping", nil)
+		a := app.App{}
+		u := user.User{
+			ID:       uuid.Nil,
+			Username: "dan@dangillis.dev",
+			Org:      org.Org{},
+			Profile:  person.Profile{},
+		}
+		adt := audit.Audit{
+			App:    a,
+			User:   u,
+			Moment: time.Now(),
+		}
+		// initialize casbin enforcer (using config files for now, will migrate to db)
+		casbinEnforcer, err := casbin.NewEnforcer("../../config/rbac_model.conf", "../../config/rbac_policy.csv")
+		if err != nil {
+			c.Fatal("casbin.NewEnforcer error")
+		}
+		ca := auth.CasbinAuthorizer{Enforcer: casbinEnforcer}
 
-	wantCtx := context.WithValue(ctx, contextKeyAccessToken, at)
-
-	tests := []struct {
-		name string
-		args args
-		want context.Context
-	}{
-		{"typical", args{ctx, at}, wantCtx},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := CtxWithAccessToken(tt.args.ctx, tt.args.token); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("CtxWithAccessToken() = %v, want %v", got, tt.want)
-			}
+		// Authorize must be tested inside a handler as it uses mux.CurrentRoute
+		testAuthorizeHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			err = ca.Authorize(lgr, r, adt)
+			c.Assert(err, qt.IsNil)
 		})
-	}
-}
 
-func TestFromRequest(t *testing.T) {
-	type args struct {
-		r *http.Request
-	}
-	token := "abcdef123"
+		rr := httptest.NewRecorder()
 
-	r, err := http.NewRequest(http.MethodGet, "/api/v1/movies", nil)
-	if err != nil {
-		t.Fatalf("http.NewRequest() error = %v", err)
-	}
-	at := AccessToken{
-		Token:     token,
-		TokenType: BearerTokenType,
-	}
+		rtr := mux.NewRouter()
+		rtr.Handle("/api/v1/ping", testAuthorizeHandler).Methods(http.MethodGet)
+		rtr.ServeHTTP(rr, req)
 
-	emptyAT := AccessToken{
-		Token:     "",
-		TokenType: BearerTokenType,
-	}
+		// If there is any issues with the Access Token, the body
+		// should be empty and the status code should be 401
+		c.Assert(rr.Code, qt.Equals, http.StatusOK)
 
-	ctx := context.Background()
-	ctx = CtxWithRealm(ctx, "DeepInTheRealm")
-	ctx = CtxWithAccessToken(ctx, at)
-	r = r.WithContext(ctx)
-
-	noAccessTokenRequest, err := http.NewRequest(http.MethodGet, "/api/v1/movies", nil)
-	if err != nil {
-		t.Fatalf("http.NewRequest() error = %v", err)
-	}
-
-	noTokenRequest, err := http.NewRequest(http.MethodGet, "/api/v1/movies", nil)
-	if err != nil {
-		t.Fatalf("http.NewRequest() error = %v", err)
-	}
-	ctx2 := context.Background()
-	ctx2 = CtxWithRealm(ctx2, "DeepInTheRealm")
-	ctx2 = CtxWithAccessToken(ctx2, emptyAT)
-	noTokenRequest = noTokenRequest.WithContext(ctx2)
-	at2 := AccessToken{
-		Token:     "",
-		TokenType: BearerTokenType,
-	}
-
-	tests := []struct {
-		name   string
-		args   args
-		want   AccessToken
-		wantOK bool
-	}{
-		{"typical", args{r: r}, at, true},
-		{"no AccessToken", args{r: noAccessTokenRequest}, AccessToken{}, false},
-		{"no token", args{r: noTokenRequest}, at2, true},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, ok := AccessTokenFromRequest(tt.args.r)
-			if ok != tt.wantOK {
-				t.Errorf("AccessTokenFromRequest() ok = %v, wantOK %v", ok, tt.wantOK)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("AccessTokenFromRequest() got = %v, want %v", got, tt.want)
-			}
-		})
-	}
+	})
 }
