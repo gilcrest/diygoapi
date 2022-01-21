db29ef58baf5f31d4d9dec9dc8f4faff84fe8ca0
Dan Gillis <gilcrest@gmail.com>
2022-01-21 16:54:17 -0500

    use constants; add missing environment vars

--- commands/env.go
+++ commands/env.go
@@ -11,31 +11,73 @@ import "os"
 //
 // This file should not be included in your git repository and should
 // be added to .gitignore. I have included it in this repository since
-// this is purely demonstrative and the password value is invalid.
+// this is purely demonstrative and the password and encryption keys are
+// invalid.
 func OverrideEnv() error {
 	var err error
 
-	err = os.Setenv("DB_NAME", "gab_local")
+	// minimum accepted log level
+	err = os.Setenv(logLevelMinEnv, "trace")
 	if err != nil {
 		return err
 	}
-	err = os.Setenv("DB_USER", "demo_user")
+
+	// log level
+	err = os.Setenv(loglevelEnv, "debug")
 	if err != nil {
 		return err
 	}
-	err = os.Setenv("DB_PASSWORD", "REPLACE_ME")
+
+	// log error stack
+	err = os.Setenv(logErrorStackEnv, "true")
 	if err != nil {
 		return err
 	}
-	err = os.Setenv("DB_HOST", "localhost")
+
+	// server port
+	err = os.Setenv(portEnv, "8080")
 	if err != nil {
 		return err
 	}
-	err = os.Setenv("DB_PORT", "5432")
+
+	// database host
+	err = os.Setenv(dbHostEnv, "localhost")
 	if err != nil {
 		return err
 	}
-	err = os.Setenv("DB_SEARCH_PATH", "demo")
+
+	// database port
+	err = os.Setenv(dbPortEnv, "5432")
+	if err != nil {
+		return err
+	}
+
+	// database name
+	err = os.Setenv(dbNameEnv, "gab_local")
+	if err != nil {
+		return err
+	}
+
+	// database user
+	err = os.Setenv(dbUserEnv, "demo_user")
+	if err != nil {
+		return err
+	}
+
+	// database user password
+	err = os.Setenv(dbPasswordEnv, "REPLACE_ME")
+	if err != nil {
+		return err
+	}
+
+	// database search path
+	err = os.Setenv(dbSearchPath, "demo")
+	if err != nil {
+		return err
+	}
+
+	// encryption key
+	err = os.Setenv(encryptKey, "REPLACE_ME")
 	if err != nil {
 		return err
 	}
