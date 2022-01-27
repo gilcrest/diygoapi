2c1596667fd682e3db6d9cafc4d3aadd0c816459
Dan Gillis <gilcrest@gmail.com>
2022-01-27 12:45:09 -0500

    cleaned up old comments

--- domain/errs/example_test.go
+++ domain/errs/example_test.go
@@ -66,19 +66,6 @@ func ExampleHTTPErrorResponse() {
 	// {"error":{"kind":"input_validation_error","code":"0212","param":"testParam","message":"Actual error message"}}
 }
 
-//func ExampleAs() {
-//	err := layer4()
-//
-//	var errsErr *errs.Error
-//	if errors.As(err, &errsErr) {
-//		fmt.Println("Error Kind:", errsErr.Kind)
-//	}
-//
-//	// Output:
-//	//
-//	// Error Kind: input_validation_error
-//}
-
 func ExampleE() {
 	err := layer4()
 	if err != nil {
@@ -108,33 +95,3 @@ func layer2() error {
 func layer1() error {
 	return errs.E(errs.Validation, errs.Parameter("testParam"), errs.Code("0212"), errors.New("Actual error message"))
 }
-
-// One alternative is to return custom error values, called sentinel errors.
-// These kind of errors can be found in the Go standard library (sql.ErrNoRows, io.EOF, etc).
-// They are useful in that they indicate if a certain kind of error has happened
-// (like a database query returning nothing), but they cannot provide any additional context,
-// so sentinel errors are not a very flexible tool.
-
-// On the other hand, they are easy to handle, since they’re based on a simple value equality:
-
-// 1	err := db.QueryRow("SELECT * FROM users WHERE id = ?", userID)
-// 2	if err == sql.ErrNoRows {
-// 3		// handle record not found error
-// 4	} else if err != nil {
-// 5		// something else went wrong
-// 6	}
-
-// func ExampleSentinelErr() {
-
-// 	// ErrFoo is for testing Sentinel Errors with errors.Is
-// 	var sentinelErr = errors.New("foo error")
-
-// 	err := sentinelErr
-
-// 	got := errors.Is(err, sentinelErr)
-
-// 	fmt.Println("Is ErrFoo:", got)
-// 	// Output:
-// 	//
-// 	// Is ErrFoo: true
-// }
