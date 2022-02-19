87e1d753cf99afeed60896cc408f54c5a3e066f3
Dan Gillis <gilcrest@gmail.com>
2022-02-19 08:46:45 -0500

    start to move over to a setup/teardown construct
    not done yet with this helper

--- domain/user/usertest/usertest.go
+++ domain/user/usertest/usertest.go
@@ -3,8 +3,11 @@
 package usertest
 
 import (
+	"context"
 	"testing"
 	"time"
+
+	"github.com/jackc/pgx/v4"
 
 	"github.com/google/uuid"
 
@@ -13,35 +16,39 @@ import (
 	"github.com/gilcrest/go-api-basic/domain/user"
 )
 
-// NewUser provides a User for testing
-func NewUser(t *testing.T) user.User {
+// Setup returns a User and teardown function for testing
+func Setup(ctx context.Context, t *testing.T, tx pgx.Tx) (user.User, func()) {
 	t.Helper()
 
+	//userstore.New(tx).FindUserByUsername(ctx)
+
 	return user.User{
-		ID:       uuid.New(),
-		Username: "otto.maddox711@gmail.com",
-		Org:      org.Org{},
-		Profile: person.Profile{
-			ID:                uuid.New(),
-			Person:            person.Person{},
-			NamePrefix:        "",
-			FirstName:         "Otto",
-			MiddleName:        "",
-			LastName:          "Maddox",
-			FullName:          "Otto Maddox",
-			NameSuffix:        "",
-			Nickname:          "",
-			CompanyName:       "",
-			CompanyDepartment: "",
-			JobTitle:          "",
-			BirthDate:         time.Date(2008, 1, 17, 0, 0, 0, 0, time.UTC),
-			LanguageID:        uuid.Nil,
-			HostedDomain:      "",
-			PictureURL:        "https://lh3.googleusercontent.com/-RYXuWxjLdxo/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucmr33m3QLTjdUatlkppT3NSN5-s8g/s96-c/photo.jpg",
-			ProfileLink:       "",
-			ProfileSource:     "",
-		},
-	}
+			ID:       uuid.New(),
+			Username: "otto.maddox711@gmail.com",
+			Org:      org.Org{},
+			Profile: person.Profile{
+				ID:                uuid.New(),
+				Person:            person.Person{},
+				NamePrefix:        "",
+				FirstName:         "Otto",
+				MiddleName:        "",
+				LastName:          "Maddox",
+				FullName:          "Otto Maddox",
+				NameSuffix:        "",
+				Nickname:          "",
+				CompanyName:       "",
+				CompanyDepartment: "",
+				JobTitle:          "",
+				BirthDate:         time.Date(2008, 1, 17, 0, 0, 0, 0, time.UTC),
+				LanguageID:        uuid.Nil,
+				HostedDomain:      "",
+				PictureURL:        "https://lh3.googleusercontent.com/-RYXuWxjLdxo/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucmr33m3QLTjdUatlkppT3NSN5-s8g/s96-c/photo.jpg",
+				ProfileLink:       "",
+				ProfileSource:     "",
+			},
+		}, func() {
+
+		}
 }
 
 // NewInvalidUser returns an invalid user defined by the method user.IsValid()
