cff460864f500cfa7f517008e45b3c49e058f7ad
Dan Gillis <gilcrest@gmail.com>
2022-02-18 23:06:46 -0500

    add Kind and more descriptions

--- domain/org/org.go
+++ domain/org/org.go
@@ -1,23 +1,31 @@
 package org
 
 import (
-	"time"
-
 	"github.com/gilcrest/go-api-basic/domain/secure"
 	"github.com/google/uuid"
 )
 
+// Kind is a way of classifying an organization. Examples are Genesis, Test, Standard
+type Kind struct {
+	// ID: The unique identifier
+	ID uuid.UUID
+	// External ID: The unique external identifier
+	ExternalID string
+	// Description: A longer description of the organization kind
+	Description string
+}
+
 // Org represents an Organization (company, institution or any other
 // organized body of people with a particular purpose)
 type Org struct {
-	ID           uuid.UUID
-	ExternalID   secure.Identifier
-	Name         string
-	Description  string
-	CreateAppID  uuid.UUID
-	CreateUserID uuid.UUID
-	CreateTime   time.Time
-	UpdateAppID  uuid.UUID
-	UpdateUserID uuid.UUID
-	UpdateTime   time.Time
+	// ID: The unique identifier
+	ID uuid.UUID
+	// External ID: The unique external identifier
+	ExternalID secure.Identifier
+	// Name: The organization name
+	Name string
+	// Description: A longer description of the organization
+	Description string
+	// Kind: a way of classifying organizations
+	Kind Kind
 }
