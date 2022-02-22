71738c968a3ba0eb223d48ce28d017fc0bd2eb9f
Dan Gillis <gilcrest@gmail.com>
2022-02-21 20:30:20 -0500

    add NewNullTime and NewNullInt32

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"net/url"
 	"strconv"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v4"
@@ -169,6 +170,18 @@ func NewNullString(s string) sql.NullString {
 	}
 }
 
+// NewNullTime returns a null if t is the zero value for time.Time,
+// otherwise it returns the time which was input
+func NewNullTime(t time.Time) sql.NullTime {
+	if t.IsZero() {
+		return sql.NullTime{}
+	}
+	return sql.NullTime{
+		Time:  t,
+		Valid: true,
+	}
+}
+
 // NewNullInt64 returns a null if i == 0, otherwise it returns
 // the int64 which was input.
 func NewNullInt64(i int64) sql.NullInt64 {
@@ -177,6 +190,18 @@ func NewNullInt64(i int64) sql.NullInt64 {
 	}
 	return sql.NullInt64{
 		Int64: i,
+		Valid: true,
+	}
+}
+
+// NewNullInt32 returns a null if i == 0, otherwise it returns
+// the int32 which was input.
+func NewNullInt32(i int32) sql.NullInt32 {
+	if i == 0 {
+		return sql.NullInt32{}
+	}
+	return sql.NullInt32{
+		Int32: i,
 		Valid: true,
 	}
 }
