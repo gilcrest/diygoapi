85f39006d45c165ebcec332bd8e40f18f16b5297
Dan Gillis <gilcrest@gmail.com>
2022-03-03 16:04:06 -0500

    simplify

--- datastore/pingstore/pingstore.go
+++ datastore/pingstore/pingstore.go
@@ -1,31 +1,20 @@
 // Package pingstore enables database health checks through the db
-// Pool Ping method. The Pinger interface allows mocking the Ping
-// when testing with no db connectivity.
+// Pool Ping method.
 package pingstore
 
 import (
 	"context"
 
 	"github.com/jackc/pgx/v4/pgxpool"
+
+	"github.com/gilcrest/go-api-basic/domain/errs"
 )
 
-// Datastorer is an interface for working with the Database
-type Datastorer interface {
-	// Pool returns *pgxpool.Pool
-	Pool() *pgxpool.Pool
-}
-
-// Pinger is the default implementation for pinging the db
-type Pinger struct {
-	Datastorer
-}
-
-// NewPinger is an initializer for DefaultPinger
-func NewPinger(ds Datastorer) Pinger {
-	return Pinger{ds}
-}
-
 // PingDB pings the DB
-func (d Pinger) PingDB(ctx context.Context) error {
-	return d.Pool().Ping(ctx)
+func PingDB(ctx context.Context, pool *pgxpool.Pool) error {
+	err := pool.Ping(ctx)
+	if err != nil {
+		return errs.E(errs.Database, err)
+	}
+	return err
 }
