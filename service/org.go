eab8b6656bb483ff62a54aec73dc47c3ca399ebf
Dan Gillis <gilcrest@gmail.com>
2022-03-11 20:01:58 -0500

    simplify to reduce db round trips

--- service/org.go
+++ service/org.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v4"
@@ -12,9 +13,16 @@ import (
 	"github.com/gilcrest/go-api-basic/domain/audit"
 	"github.com/gilcrest/go-api-basic/domain/errs"
 	"github.com/gilcrest/go-api-basic/domain/org"
+	"github.com/gilcrest/go-api-basic/domain/person"
 	"github.com/gilcrest/go-api-basic/domain/secure"
 	"github.com/gilcrest/go-api-basic/domain/user"
 )
+
+// orgAudit is the combination of a domain Org and its audit data
+type orgAudit struct {
+	Org         org.Org
+	SimpleAudit audit.SimpleAudit
+}
 
 // CreateOrgRequest is the request struct for Creating an Org
 type CreateOrgRequest struct {
@@ -25,33 +33,48 @@ type CreateOrgRequest struct {
 
 // OrgResponse is the response struct for an Org
 type OrgResponse struct {
-	ExternalID  string        `json:"external_id"`
-	Name        string        `json:"name"`
-	Kind        string        `json:"kind"`
-	Description string        `json:"description"`
-	CreateAudit auditResponse `json:"create_audit"`
-	UpdateAudit auditResponse `json:"update_audit"`
+	ExternalID          string `json:"external_id"`
+	Name                string `json:"name"`
+	KindExternalID      string `json:"kind_description"`
+	Description         string `json:"description"`
+	CreateAppExtlID     string `json:"create_app_extl_id"`
+	CreateUsername      string `json:"create_username"`
+	CreateUserFirstName string `json:"create_user_first_name"`
+	CreateUserLastName  string `json:"create_user_last_name"`
+	CreateDateTime      string `json:"create_date_time"`
+	UpdateAppExtlID     string `json:"update_app_extl_id"`
+	UpdateUsername      string `json:"update_username"`
+	UpdateUserFirstName string `json:"update_user_first_name"`
+	UpdateUserLastName  string `json:"update_user_last_name"`
+	UpdateDateTime      string `json:"update_date_time"`
 }
 
 // newOrgResponse initializes OrgResponse given an org.Org.
-func newOrgResponse(o org.Org, sa audit.SimpleAudit) OrgResponse {
+func newOrgResponse(oa orgAudit) OrgResponse {
 	return OrgResponse{
-		ExternalID:  o.ExternalID.String(),
-		Name:        o.Name,
-		Description: o.Description,
-		Kind:        o.Kind.ExternalID,
-		CreateAudit: newAuditResponse(sa.First),
-		UpdateAudit: newAuditResponse(sa.Last),
+		ExternalID:          oa.Org.ExternalID.String(),
+		Name:                oa.Org.Name,
+		Description:         oa.Org.Description,
+		KindExternalID:      oa.Org.Kind.ExternalID,
+		CreateAppExtlID:     oa.SimpleAudit.First.App.ExternalID.String(),
+		CreateUsername:      oa.SimpleAudit.First.User.Username,
+		CreateUserFirstName: oa.SimpleAudit.First.User.Profile.FirstName,
+		CreateUserLastName:  oa.SimpleAudit.First.User.Profile.LastName,
+		CreateDateTime:      oa.SimpleAudit.First.Moment.Format(time.RFC3339),
+		UpdateAppExtlID:     oa.SimpleAudit.Last.App.ExternalID.String(),
+		UpdateUsername:      oa.SimpleAudit.Last.User.Username,
+		UpdateUserFirstName: oa.SimpleAudit.Last.User.Profile.FirstName,
+		UpdateUserLastName:  oa.SimpleAudit.Last.User.Profile.LastName,
 	}
 }
 
-// CreateOrgService is a service for creating an Org
-type CreateOrgService struct {
+// OrgService is a service for creating, reading, updating and deleting an Org
+type OrgService struct {
 	Datastorer Datastorer
 }
 
 // Create is used to create an Org
-func (s CreateOrgService) Create(ctx context.Context, r *CreateOrgRequest, adt audit.Audit) (OrgResponse, error) {
+func (s OrgService) Create(ctx context.Context, r *CreateOrgRequest, adt audit.Audit) (OrgResponse, error) {
 	var err error
 
 	var kind org.Kind
@@ -74,13 +97,19 @@ func (s CreateOrgService) Create(ctx context.Context, r *CreateOrgRequest, adt a
 		Last:  adt,
 	}
 
+	oa := orgAudit{
+		Org:         o,
+		SimpleAudit: sa,
+	}
+
 	// start db txn using pgxpool
-	tx, err := s.Datastorer.BeginTx(ctx)
+	var tx pgx.Tx
+	tx, err = s.Datastorer.BeginTx(ctx)
 	if err != nil {
 		return OrgResponse{}, err
 	}
 
-	err = createOrgDB(ctx, s.Datastorer, tx, o, sa)
+	err = createOrgDB(ctx, s.Datastorer, tx, oa)
 	if err != nil {
 		return OrgResponse{}, err
 	}
@@ -91,17 +120,18 @@ func (s CreateOrgService) Create(ctx context.Context, r *CreateOrgRequest, adt a
 		return OrgResponse{}, err
 	}
 
-	return newOrgResponse(o, sa), nil
+	return newOrgResponse(oa), nil
 }
 
-// created separate function as it's used by genesis service as well
-func createOrgDB(ctx context.Context, ds Datastorer, tx pgx.Tx, o org.Org, sa audit.SimpleAudit) error {
-	if o.Kind.ID == uuid.Nil {
+// createOrgDB writes an Org and its audit information to the database.
+// separate function as it's used by genesis service as well
+func createOrgDB(ctx context.Context, ds Datastorer, tx pgx.Tx, oa orgAudit) error {
+	if oa.Org.Kind.ID == uuid.Nil {
 		return errs.E(errs.Database, ds.RollbackTx(ctx, tx, errs.E("org Kind is required")))
 	}
 
 	// create database record using orgstore
-	_, err := orgstore.New(tx).CreateOrg(ctx, newCreateOrgParams(o, sa))
+	_, err := orgstore.New(tx).CreateOrg(ctx, newCreateOrgParams(oa))
 	if err != nil {
 		return errs.E(errs.Database, ds.RollbackTx(ctx, tx, err))
 	}
@@ -110,31 +140,19 @@ func createOrgDB(ctx context.Context, ds Datastorer, tx pgx.Tx, o org.Org, sa au
 }
 
 // newCreateOrgParams maps an Org to orgstore.CreateOrgParams
-func newCreateOrgParams(o org.Org, simpleAudit audit.SimpleAudit) orgstore.CreateOrgParams {
+func newCreateOrgParams(oa orgAudit) orgstore.CreateOrgParams {
 	return orgstore.CreateOrgParams{
-		OrgID:           o.ID,
-		OrgExtlID:       o.ExternalID.String(),
-		OrgName:         o.Name,
-		OrgDescription:  o.Description,
-		OrgKindID:       o.Kind.ID,
-		CreateAppID:     simpleAudit.First.App.ID,
-		CreateUserID:    datastore.NewNullUUID(simpleAudit.First.User.ID),
-		CreateTimestamp: simpleAudit.First.Moment,
-		UpdateAppID:     simpleAudit.Last.App.ID,
-		UpdateUserID:    datastore.NewNullUUID(simpleAudit.Last.User.ID),
-		UpdateTimestamp: simpleAudit.Last.Moment,
-	}
-}
-
-// newUpdateOrgParams maps an Org to orgstore.UpdateOrgParams
-func newUpdateOrgParams(o org.Org, ua audit.Audit) orgstore.UpdateOrgParams {
-	return orgstore.UpdateOrgParams{
-		OrgID:           o.ID,
-		OrgName:         o.Name,
-		OrgDescription:  o.Description,
-		UpdateAppID:     ua.App.ID,
-		UpdateUserID:    datastore.NewNullUUID(ua.User.ID),
-		UpdateTimestamp: ua.Moment,
+		OrgID:           oa.Org.ID,
+		OrgExtlID:       oa.Org.ExternalID.String(),
+		OrgName:         oa.Org.Name,
+		OrgDescription:  oa.Org.Description,
+		OrgKindID:       oa.Org.Kind.ID,
+		CreateAppID:     oa.SimpleAudit.First.App.ID,
+		CreateUserID:    datastore.NewNullUUID(oa.SimpleAudit.First.User.ID),
+		CreateTimestamp: oa.SimpleAudit.First.Moment,
+		UpdateAppID:     oa.SimpleAudit.Last.App.ID,
+		UpdateUserID:    datastore.NewNullUUID(oa.SimpleAudit.Last.User.ID),
+		UpdateTimestamp: oa.SimpleAudit.Last.Moment,
 	}
 }
 
@@ -145,174 +163,300 @@ type UpdateOrgRequest struct {
 	Description string `json:"description"`
 }
 
-// UpdateOrgService is a service for updating an Org
-type UpdateOrgService struct {
-	Datastorer Datastorer
-}
-
 // Update is used to update an Org
-func (cos UpdateOrgService) Update(ctx context.Context, r *UpdateOrgRequest, adt audit.Audit) (OrgResponse, error) {
+func (s OrgService) Update(ctx context.Context, r *UpdateOrgRequest, adt audit.Audit) (OrgResponse, error) {
 	// start db txn using pgxpool
-	tx, err := cos.Datastorer.BeginTx(ctx)
+	tx, err := s.Datastorer.BeginTx(ctx)
 	if err != nil {
 		return OrgResponse{}, err
 	}
 
 	// retrieve existing Org
-	o, sa, err := findOrgByExternalID(ctx, tx, r.ExternalID, true)
+	var oa orgAudit
+	oa, err = findOrgByExternalIDWithAudit(ctx, tx, r.ExternalID)
 	if err != nil {
 		if err == pgx.ErrNoRows {
 			return OrgResponse{}, errs.E(errs.Validation, "No org exists for the given external ID")
 		}
 		return OrgResponse{}, errs.E(errs.Database, err)
 	}
+	// overwrite Last audit with the current audit
+	oa.SimpleAudit.Last = adt
 
 	// override fields with data from request
-	o.Name = r.Name
-	o.Description = r.Description
+	oa.Org.Name = r.Name
+	oa.Org.Description = r.Description
+
+	params := orgstore.UpdateOrgParams{
+		OrgID:           oa.Org.ID,
+		OrgName:         oa.Org.Name,
+		OrgDescription:  oa.Org.Description,
+		UpdateAppID:     adt.App.ID,
+		UpdateUserID:    datastore.NewNullUUID(adt.User.ID),
+		UpdateTimestamp: adt.Moment,
+	}
 
 	// update database record using orgstore
-	err = orgstore.New(tx).UpdateOrg(ctx, newUpdateOrgParams(o, adt))
+	err = orgstore.New(tx).UpdateOrg(ctx, params)
 	if err != nil {
-		return OrgResponse{}, errs.E(errs.Database, cos.Datastorer.RollbackTx(ctx, tx, err))
+		return OrgResponse{}, errs.E(errs.Database, s.Datastorer.RollbackTx(ctx, tx, err))
 	}
 
 	// commit db txn using pgxpool
-	err = cos.Datastorer.CommitTx(ctx, tx)
+	err = s.Datastorer.CommitTx(ctx, tx)
 	if err != nil {
 		return OrgResponse{}, err
 	}
 
-	sa.Last = adt
-	or := newOrgResponse(o, *sa)
-
-	return or, nil
+	return newOrgResponse(oa), nil
 }
 
-// FindOrgService interface reads Orgs form the datastore
-type FindOrgService struct {
-	Datastorer Datastorer
-}
+// Delete is used to delete an Org
+func (s OrgService) Delete(ctx context.Context, extlID string) (DeleteResponse, error) {
 
-// FindAll is used to list all orgs in the datastore
-func (fos FindOrgService) FindAll(ctx context.Context) ([]OrgResponse, error) {
-
-	var (
-		dbos     []orgstore.Org
-		response []OrgResponse
-		err      error
-	)
-
-	dbtx := fos.Datastorer.Pool()
-	dbos, err = orgstore.New(dbtx).FindOrgs(ctx)
+	// start db txn using pgxpool
+	tx, err := s.Datastorer.BeginTx(ctx)
 	if err != nil {
-		return nil, errs.E(errs.Database, err)
+		return DeleteResponse{}, err
 	}
 
-	for _, dbo := range dbos {
-		var o org.Org
-		sa := new(audit.SimpleAudit)
-		o, sa, err = hydrateOrgFromDB(ctx, dbtx, dbo, true)
-		if err != nil {
-			return nil, err
+	// retrieve existing Org
+	o, err := findOrgByExternalID(ctx, tx, extlID)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			return DeleteResponse{}, errs.E(errs.Validation, "No org exists for the given external ID")
 		}
-		or := newOrgResponse(o, *sa)
+		return DeleteResponse{}, errs.E(errs.Database, err)
+	}
 
-		response = append(response, or)
+	err = orgstore.New(tx).DeleteOrg(ctx, o.ID)
+	if err != nil {
+		return DeleteResponse{}, errs.E(errs.Database, s.Datastorer.RollbackTx(ctx, tx, err))
+	}
+
+	// commit db txn using pgxpool
+	err = s.Datastorer.CommitTx(ctx, tx)
+	if err != nil {
+		return DeleteResponse{}, err
+	}
+
+	response := DeleteResponse{
+		ExternalID: extlID,
+		Deleted:    true,
 	}
 
 	return response, nil
 }
 
+// FindAll is used to list all orgs in the datastore
+func (s OrgService) FindAll(ctx context.Context) ([]OrgResponse, error) {
+
+	var (
+		dbos      []orgstore.FindOrgsWithAuditRow
+		responses []OrgResponse
+		err       error
+	)
+
+	dbtx := s.Datastorer.Pool()
+	dbos, err = orgstore.New(dbtx).FindOrgsWithAudit(ctx)
+	if err != nil {
+		return nil, errs.E(errs.Database, err)
+	}
+
+	for _, row := range dbos {
+		o := org.Org{
+			ID:          row.OrgID,
+			ExternalID:  secure.MustParseIdentifier(row.OrgExtlID),
+			Name:        row.OrgName,
+			Description: row.OrgDescription,
+			Kind: org.Kind{
+				ID:          row.OrgKindID,
+				ExternalID:  row.OrgKindExtlID,
+				Description: row.OrgKindDesc,
+			},
+		}
+
+		sa := audit.SimpleAudit{
+			First: audit.Audit{
+				App: app.App{
+					ID:          row.CreateAppID,
+					ExternalID:  secure.MustParseIdentifier(row.CreateAppExtlID),
+					Org:         org.Org{ID: row.CreateAppOrgID},
+					Name:        row.CreateAppName,
+					Description: row.CreateAppDescription,
+					APIKeys:     nil,
+				},
+				User: user.User{
+					ID:       row.CreateUserID.UUID,
+					Username: row.CreateUsername,
+					Org:      org.Org{ID: row.CreateUserOrgID},
+					Profile: person.Profile{
+						FirstName: row.CreateUserFirstName,
+						LastName:  row.CreateUserLastName,
+					},
+				},
+				Moment: row.CreateTimestamp,
+			},
+			Last: audit.Audit{
+				App: app.App{
+					ID:          row.UpdateAppID,
+					ExternalID:  secure.MustParseIdentifier(row.UpdateAppExtlID),
+					Org:         org.Org{ID: row.UpdateAppOrgID},
+					Name:        row.UpdateAppName,
+					Description: row.UpdateAppDescription,
+					APIKeys:     nil,
+				},
+				User: user.User{
+					ID:       row.UpdateUserID.UUID,
+					Username: row.UpdateUsername,
+					Org:      org.Org{ID: row.UpdateUserOrgID},
+					Profile: person.Profile{
+						FirstName: row.UpdateUserFirstName,
+						LastName:  row.UpdateUserLastName,
+					},
+				},
+				Moment: row.UpdateTimestamp,
+			},
+		}
+		or := newOrgResponse(orgAudit{Org: o, SimpleAudit: sa})
+
+		responses = append(responses, or)
+	}
+
+	return responses, nil
+}
+
 // FindByExternalID is used to find an Org by its External ID
-func (fos FindOrgService) FindByExternalID(ctx context.Context, extlID string) (OrgResponse, error) {
+func (s OrgService) FindByExternalID(ctx context.Context, extlID string) (OrgResponse, error) {
 
-	dbtx := fos.Datastorer.Pool()
+	dbtx := s.Datastorer.Pool()
 
-	o, sa, err := findOrgByExternalID(ctx, dbtx, extlID, true)
+	oa, err := findOrgByExternalIDWithAudit(ctx, dbtx, extlID)
 	if err != nil {
 		return OrgResponse{}, err
 	}
 
-	return newOrgResponse(o, *sa), nil
+	return newOrgResponse(oa), nil
 }
 
 // findOrgByID retrieves an Org from the datastore given a unique ID
-func findOrgByID(ctx context.Context, dbtx DBTX, id uuid.UUID, withAudit bool) (org.Org, *audit.SimpleAudit, error) {
+func findOrgByID(ctx context.Context, dbtx DBTX, id uuid.UUID) (org.Org, error) {
 	dbo, err := orgstore.New(dbtx).FindOrgByID(ctx, id)
 	if err != nil {
-		return org.Org{}, nil, errs.E(errs.Database, err)
-	}
-
-	return hydrateOrgFromDB(ctx, dbtx, dbo, withAudit)
-}
-
-// findOrgByExternalID retrieves an Org from the datastore given a unique external ID
-// if withAudit is set to true, audit.SimpleAudit will be returned, otherwise nil is returned
-func findOrgByExternalID(ctx context.Context, dbtx DBTX, extlID string, withAudit bool) (org.Org, *audit.SimpleAudit, error) {
-	dbo, err := orgstore.New(dbtx).FindOrgByExtlID(ctx, extlID)
-	if err != nil {
-		return org.Org{}, nil, errs.E(errs.Database, err)
-	}
-
-	return hydrateOrgFromDB(ctx, dbtx, dbo, withAudit)
-}
-
-// hydrateOrgFromDB populates an org.Org and an audit.SimpleAudit (if withAudit is true) given an orgstore.Org
-func hydrateOrgFromDB(ctx context.Context, dbtx DBTX, dbo orgstore.Org, withAudit bool) (org.Org, *audit.SimpleAudit, error) {
-	var (
-		extl                   secure.Identifier
-		err                    error
-		createApp, updateApp   app.App
-		createUser, updateUser user.User
-	)
-
-	extl, err = secure.ParseIdentifier(dbo.OrgExtlID)
-	if err != nil {
-		return org.Org{}, nil, err
+		return org.Org{}, errs.E(errs.Database, err)
 	}
 
 	o := org.Org{
 		ID:          dbo.OrgID,
-		ExternalID:  extl,
+		ExternalID:  secure.MustParseIdentifier(dbo.OrgExtlID),
 		Name:        dbo.OrgName,
 		Description: dbo.OrgDescription,
+		Kind: org.Kind{
+			ID:          dbo.OrgKindID,
+			ExternalID:  dbo.OrgKindExtlID,
+			Description: dbo.OrgKindDesc,
+		},
 	}
 
-	sa := new(audit.SimpleAudit)
-	if withAudit {
-		createApp, _, err = findAppByID(ctx, dbtx, dbo.CreateAppID, false)
-		if err != nil {
-			return org.Org{}, nil, err
-		}
-		createUser, err = findUserByID(ctx, dbtx, dbo.CreateUserID.UUID)
-		if err != nil {
-			return org.Org{}, nil, err
-		}
-		createAudit := audit.Audit{
-			App:    createApp,
-			User:   createUser,
-			Moment: dbo.CreateTimestamp,
-		}
+	return o, nil
+}
 
-		updateApp, _, err = findAppByID(ctx, dbtx, dbo.UpdateAppID, false)
-		if err != nil {
-			return org.Org{}, nil, err
-		}
-		updateUser, err = findUserByID(ctx, dbtx, dbo.UpdateUserID.UUID)
-		if err != nil {
-			return org.Org{}, nil, err
-		}
-		updateAudit := audit.Audit{
-			App:    updateApp,
-			User:   updateUser,
-			Moment: dbo.UpdateTimestamp,
-		}
+// findOrgByExternalID retrieves an Org from the datastore given a unique external ID
+func findOrgByExternalID(ctx context.Context, dbtx DBTX, extlID string) (org.Org, error) {
+	var (
+		row orgstore.FindOrgByExtlIDRow
+		err error
+	)
 
-		sa = &audit.SimpleAudit{First: createAudit, Last: updateAudit}
+	row, err = orgstore.New(dbtx).FindOrgByExtlID(ctx, extlID)
+	if err != nil {
+		return org.Org{}, errs.E(errs.Database, err)
 	}
 
-	return o, sa, nil
+	o := org.Org{
+		ID:          row.OrgID,
+		ExternalID:  secure.MustParseIdentifier(row.OrgExtlID),
+		Name:        row.OrgName,
+		Description: row.OrgDescription,
+		Kind: org.Kind{
+			ID:          row.OrgKindID,
+			ExternalID:  row.OrgKindExtlID,
+			Description: row.OrgKindDesc,
+		},
+	}
+
+	return o, nil
+}
+
+// findOrgByExternalID retrieves Org data from the datastore given a unique external ID.
+// This data is then hydrated into the org.Org struct along with the simple audit struct
+func findOrgByExternalIDWithAudit(ctx context.Context, dbtx DBTX, extlID string) (orgAudit, error) {
+	var (
+		row orgstore.FindOrgByExtlIDWithAuditRow
+		err error
+	)
+
+	row, err = orgstore.New(dbtx).FindOrgByExtlIDWithAudit(ctx, extlID)
+	if err != nil {
+		return orgAudit{}, errs.E(errs.Database, err)
+	}
+
+	o := org.Org{
+		ID:          row.OrgID,
+		ExternalID:  secure.MustParseIdentifier(row.OrgExtlID),
+		Name:        row.OrgName,
+		Description: row.OrgDescription,
+		Kind: org.Kind{
+			ID:          row.OrgKindID,
+			ExternalID:  row.OrgKindExtlID,
+			Description: row.OrgKindDesc,
+		},
+	}
+
+	sa := audit.SimpleAudit{
+		First: audit.Audit{
+			App: app.App{
+				ID:          row.CreateAppID,
+				ExternalID:  secure.MustParseIdentifier(row.CreateAppExtlID),
+				Org:         org.Org{ID: row.CreateAppOrgID},
+				Name:        row.CreateAppName,
+				Description: row.CreateAppDescription,
+				APIKeys:     nil,
+			},
+			User: user.User{
+				ID:       row.CreateUserID.UUID,
+				Username: row.CreateUsername,
+				Org:      org.Org{ID: row.CreateUserOrgID},
+				Profile: person.Profile{
+					FirstName: row.CreateUserFirstName,
+					LastName:  row.CreateUserLastName,
+				},
+			},
+			Moment: row.CreateTimestamp,
+		},
+		Last: audit.Audit{
+			App: app.App{
+				ID:          row.UpdateAppID,
+				ExternalID:  secure.MustParseIdentifier(row.UpdateAppExtlID),
+				Org:         org.Org{ID: row.UpdateAppOrgID},
+				Name:        row.UpdateAppName,
+				Description: row.UpdateAppDescription,
+				APIKeys:     nil,
+			},
+			User: user.User{
+				ID:       row.UpdateUserID.UUID,
+				Username: row.UpdateUsername,
+				Org:      org.Org{ID: row.UpdateUserOrgID},
+				Profile: person.Profile{
+					FirstName: row.UpdateUserFirstName,
+					LastName:  row.UpdateUserLastName,
+				},
+			},
+			Moment: row.UpdateTimestamp,
+		},
+	}
+
+	return orgAudit{Org: o, SimpleAudit: sa}, nil
 }
 
 // findOrgKindByExtlID finds an org kind from the datastore given its External ID
