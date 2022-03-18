79fbadee09b33f623ddf4bd62214a86694360179
Dan Gillis <gilcrest@gmail.com>
2022-03-18 13:13:11 -0400

    add app_name ui; add column comments

--- scripts/db/migrations/up/001-app.sql
+++ scripts/db/migrations/up/001-app.sql
@@ -5,7 +5,6 @@ create table demo.app
     app_extl_id      varchar   not null,
     app_name         varchar   not null,
     app_description  varchar   not null,
-    active           boolean,
     create_app_id    uuid      not null,
     create_user_id   uuid,
     create_timestamp timestamp not null,
@@ -22,9 +21,33 @@ create table demo.app
 
 comment on table demo.app is 'app stores data about applications that interact with the system';
 
+comment on column demo.app.app_id is 'The Unique ID for the table.';
+
+comment on column demo.app.org_id is 'The organization ID for the organization that the app belongs to.';
+
+comment on column demo.app.app_extl_id is 'The unique application External ID to be given to outside callers.';
+
+comment on column demo.app.app_name is 'The application name is a short name for the application.';
+
+comment on column demo.app.app_description is 'The application description is several sentences to describe the application.';
+
+comment on column demo.app.create_app_id is 'The application which created this record.';
+
+comment on column demo.app.create_user_id is 'The user which created this record.';
+
+comment on column demo.app.create_timestamp is 'The timestamp when this record was created.';
+
+comment on column demo.app.update_app_id is 'The application which performed the most recent update to this record.';
+
+comment on column demo.app.update_user_id is 'The user which performed the most recent update to this record.';
+
+comment on column demo.app.update_timestamp is 'The timestamp when the record was updated most recently.';
+
 alter table demo.app
     owner to demo_user;
 
 create unique index app_app_extl_id_uindex
     on demo.app (app_extl_id);
 
+create unique index app_name_uindex
+    on demo.app (org_id, app_name);
--- scripts/db/objects/demo/app.sql
+++ scripts/db/objects/demo/app.sql
@@ -5,7 +5,6 @@ create table app
     app_extl_id      varchar   not null,
     app_name         varchar   not null,
     app_description  varchar   not null,
-    active           boolean,
     create_app_id    uuid      not null,
     create_user_id   uuid,
     create_timestamp timestamp not null,
@@ -31,9 +30,34 @@ create table app
 
 comment on table app is 'app stores data about applications that interact with the system';
 
+comment on column app.app_id is 'The Unique ID for the table.';
+
+comment on column app.org_id is 'The organization ID for the organization that the app belongs to.';
+
+comment on column app.app_extl_id is 'The unique application External ID to be given to outside callers.';
+
+comment on column app.app_name is 'The application name is a short name for the application.';
+
+comment on column app.app_description is 'The application description is several sentences to describe the application.';
+
+comment on column app.create_app_id is 'The application which created this record.';
+
+comment on column app.create_user_id is 'The user which created this record.';
+
+comment on column app.create_timestamp is 'The timestamp when this record was created.';
+
+comment on column app.update_app_id is 'The application which performed the most recent update to this record.';
+
+comment on column app.update_user_id is 'The user which performed the most recent update to this record.';
+
+comment on column app.update_timestamp is 'The timestamp when the record was updated most recently.';
+
 alter table app
     owner to demo_user;
 
 create unique index app_app_extl_id_uindex
     on app (app_extl_id);
+
+create unique index app_name_uindex
+    on app (org_id, app_name);
 
