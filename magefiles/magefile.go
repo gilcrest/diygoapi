bb24e134fbb9da468e76710554cb4ceec3048b3c
Dan Gillis <gilcrest@gmail.com>
2022-04-18 21:24:58 -0400

    allow passing env as well as gcp code

--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -1,9 +1,13 @@
 package main
 
 import (
-	"github.com/magefile/mage/sh"
+	"fmt"
+	"os"
 
-	"github.com/gilcrest/go-api-basic/commands"
+	"github.com/magefile/mage/sh"
+	"github.com/pkg/errors"
+
+	"github.com/gilcrest/go-api-basic/command"
 )
 
 // DBUp uses the psql command line interface to execute DDL scripts
@@ -12,18 +16,15 @@ import (
 // Check output to determine if any errors occurred.
 // Eventually, I will write this to stop on errors, but for now it is
 // what it is.
-func DBUp() error {
-	var (
-		err  error
-		args []string
-	)
+func DBUp(env string) (err error) {
+	var args []string
 
-	err = commands.OverrideEnv()
+	err = command.LoadEnv(command.ParseEnv(env))
 	if err != nil {
 		return err
 	}
 
-	args, err = commands.PSQLArgs(true)
+	args, err = command.PSQLArgs(true)
 	if err != nil {
 		return err
 	}
@@ -42,18 +43,15 @@ func DBUp() error {
 // an individual file. Check output to determine if any errors occurred.
 // Eventually, I will write this to stop on errors, but for now it is
 // what it is.
-func DBDown() error {
-	var (
-		err  error
-		args []string
-	)
+func DBDown(env string) (err error) {
+	var args []string
 
-	err = commands.OverrideEnv()
+	err = command.LoadEnv(command.ParseEnv(env))
 	if err != nil {
 		return err
 	}
 
-	args, err = commands.PSQLArgs(false)
+	args, err = command.PSQLArgs(false)
 	if err != nil {
 		return err
 	}
@@ -67,8 +65,8 @@ func DBDown() error {
 }
 
 // TestAll runs all tests for the app
-func TestAll() error {
-	err := commands.OverrideEnv()
+func TestAll(env string) (err error) {
+	err = command.LoadEnv(command.ParseEnv(env))
 	if err != nil {
 		return err
 	}
@@ -81,29 +79,14 @@ func TestAll() error {
 	return nil
 }
 
-// Build creates the binary executable with name srvr
-func Build() error {
-	err := sh.Run("go", "build", "-o", "srvr")
+// Run runs program using the given environment configuration
+func Run(env string) (err error) {
+	err = command.LoadEnv(command.ParseEnv(env))
 	if err != nil {
 		return err
 	}
 
-	return nil
-}
-
-// Run runs the binary executable created with Build
-func Run() (err error) {
-	err = commands.OverrideEnv()
-	if err != nil {
-		return err
-	}
-
-	err = sh.Run("go", "build", "-o", "srvr")
-	if err != nil {
-		return err
-	}
-
-	err = sh.Run("./srvr")
+	err = sh.Run("go", "run", "main.go")
 	if err != nil {
 		return err
 	}
@@ -112,13 +95,13 @@ func Run() (err error) {
 }
 
 // Genesis runs all tests including executing the Genesis service
-func Genesis() (err error) {
-	err = commands.OverrideEnv()
+func Genesis(env string) (err error) {
+	err = command.LoadEnv(command.ParseEnv(env))
 	if err != nil {
 		return err
 	}
 
-	err = commands.Genesis()
+	err = command.Genesis()
 	if err != nil {
 		return err
 	}
@@ -128,5 +111,157 @@ func Genesis() (err error) {
 
 // NewKey generates a new encryption key
 func NewKey() {
-	commands.NewEncryptionKey()
+	command.NewEncryptionKey()
+}
+
+// GCP deploys the app to Google Cloud Run
+func GCP(env string) error {
+
+	f, err := command.NewConfigFile(command.ParseEnv(env))
+	if err != nil {
+		return err
+	}
+
+	image := command.GCPArtifactRegistryContainerImage{
+		ProjectID:          f.Config.GCP.ProjectID,
+		RepositoryLocation: f.Config.GCP.ArtifactoryRegistry.RepoLocation,
+		RepositoryName:     f.Config.GCP.ArtifactoryRegistry.RepoName,
+		ImageName:          f.Config.GCP.ArtifactoryRegistry.ImageID,
+		ImageTag:           f.Config.GCP.ArtifactoryRegistry.Tag,
+	}
+
+	err = gcpArtifactRegistryBuild(image)
+	if err != nil {
+		return err
+	}
+
+	args := command.GCPCloudRunDeployImage(f, image)
+	if err != nil {
+		return err
+	}
+
+	err = sh.Run("gcloud", args...)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func gcpArtifactRegistryBuild(image command.GCPArtifactRegistryContainerImage) error {
+	const (
+		dockerfileOrigin      = "./magefiles/Dockerfile"
+		dockerfileDestination = "Dockerfile"
+	)
+	var err error
+
+	// move the Dockerfile to the project root directory
+	err = os.Rename(dockerfileOrigin, dockerfileDestination)
+	if err != nil {
+		return err
+	}
+	var cwd string
+	cwd, err = os.Getwd()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Dockerfile moved from %s to %s\n", dockerfileOrigin, cwd)
+
+	// defer moving the Dockerfile back
+	defer func() {
+		deferErr := os.Rename(dockerfileDestination, dockerfileOrigin)
+		if deferErr != nil {
+			if err != nil {
+				err = errors.Wrap(err, deferErr.Error())
+				return
+			}
+			err = deferErr
+			return
+		}
+		fmt.Printf("Dockerfile moved back to %s\n", dockerfileOrigin)
+	}()
+
+	// args for gcloud
+	args := []string{"builds", "submit", "--tag", image.String()}
+
+	err = sh.Run("gcloud", args...)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GenConfig generates configuration files for the given environment.
+// The files are run through cue vet first to ensure they are acceptable
+// given the schema.
+//
+// Acceptable environment values are: local, staging, production
+func GenConfig(env string) (err error) {
+
+	var paths command.ConfigCueFilePaths
+	paths, err = command.CUEPaths(command.ParseEnv(env))
+	if err != nil {
+		return err
+	}
+
+	// Vet input files
+	vetArgs := []string{"vet"}
+	for _, path := range paths.Input {
+		vetArgs = append(vetArgs, path)
+	}
+	err = sh.Run("cue", vetArgs...)
+	if err != nil {
+		return err
+	}
+
+	// Export output files
+	exportArgs := []string{"export"}
+	for _, path := range paths.Input {
+		exportArgs = append(exportArgs, path)
+	}
+	exportArgs = append(exportArgs, "--force", "--out", "json", "--outfile", paths.Output)
+
+	err = sh.Run("cue", exportArgs...)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// StartGCPDB starts the GCP Cloud SQL database for the environment/config given
+func StartGCPDB(env string) (err error) {
+	var f command.ConfigFile
+	f, err = command.NewConfigFile(command.ParseEnv(env))
+	if err != nil {
+		return err
+	}
+
+	args := []string{"sql", "instances", "patch", f.Config.GCP.CloudSQL.InstanceName, "--activation-policy=ALWAYS"}
+
+	err = sh.Run("gcloud", args...)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// StopGCPDB stops the GCP Cloud SQL database for the environment/config given
+func StopGCPDB(env string) (err error) {
+	var f command.ConfigFile
+	f, err = command.NewConfigFile(command.ParseEnv(env))
+	if err != nil {
+		return err
+	}
+
+	args := []string{"sql", "instances", "patch", f.Config.GCP.CloudSQL.InstanceName, "--activation-policy=NEVER"}
+
+	err = sh.Run("gcloud", args...)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
