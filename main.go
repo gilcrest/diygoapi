61954a55f279d7dadf766219e57c4e568d6a9abe
Dan Gillis <gilcrest@gmail.com>
2022-04-18 21:30:28 -0400

    new command directory

--- main.go
+++ main.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/gilcrest/go-api-basic/commands"
+	"github.com/gilcrest/go-api-basic/command"
 )
 
 func main() {
-	if err := commands.Run(os.Args); err != nil {
+	if err := command.Run(os.Args); err != nil {
 		fmt.Fprintf(os.Stderr, "error from commands.Run(): %s\n", err)
 		os.Exit(1)
 	}
